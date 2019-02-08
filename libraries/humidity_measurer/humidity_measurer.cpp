b26e50eeeb8324a276557c0ce5e8cbfdfab64ace
Ian <ian@somewhere.com>
2019-02-08 03:08:25 +0300

    [^] [lib.humidity_measurer] Better heuristic in humidity measure.
    Done real-world experiments and adjusted code accordingly.

--- libraries/humidity_measurer/humidity_measurer.cpp
+++ libraries/humidity_measurer/humidity_measurer.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include "humidity_measurer.h"
 
-const uint32_t line_problem_threshold = 10;
-
 humidity_measurer::humidity_measurer() {
   sensor_pin = A0;
   power_pin = 2;
@@ -14,26 +12,38 @@ humidity_measurer::humidity_measurer() {
 }
 
 int humidity_measurer::get_raw_value() {
-  int raw_value;
+  const float measure_period = 3.8;
+  const uint8_t num_measures = 20;
+  const uint16_t measure_delay = measure_period * 1000 / num_measures;
+  const uint16_t line_problem_threshold = 10;
+  const uint16_t pre_measures_delay = 50;
+
   if (power_off_between_measures) {
     digitalWrite(power_pin, HIGH);
-    delay(50);
+    delay(pre_measures_delay);
+  }
+
+  uint16_t raw_value;
+  uint32_t sum = 0;
+  for (uint8_t i = 1; i <= num_measures; i++) {
     raw_value = analogRead(sensor_pin);
+    delay(measure_delay);
+    sum += raw_value;
+  }
+  raw_value = sum / num_measures;
+
+  if (power_off_between_measures)
     digitalWrite(power_pin, LOW);
-  }
-  else {
-    raw_value = analogRead(sensor_pin);
-  }
 
   is_line_problem = (raw_value <= line_problem_threshold);
 
   return raw_value;
 }
 
-const int aligned_low = 0;
-const int aligned_high = 100;
-
 int humidity_measurer::get_value() {
+  const int aligned_low = 0;
+  const int aligned_high = 100;
+
   int result;
   result = get_raw_value();
   result = constrain(result, min_value, max_value);
--- libraries/humidity_measurer/humidity_measurer.h
+++ libraries/humidity_measurer/humidity_measurer.h
@@ -3,13 +3,15 @@
 class humidity_measurer {
   public:
     int sensor_pin;
+    int power_pin;
+
     int min_value;
     int max_value;
     bool power_off_between_measures;
     bool high_means_dry;
-    int power_pin;
+
     humidity_measurer();
-    int get_raw_value();
     int get_value();
+    int get_raw_value();
     bool is_line_problem;
 };
