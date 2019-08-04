465e39e70c91d0360ab4211503154aba82365ec0
Ian <ian@somewhere.com>
2019-08-04 16:01:49 +0300

    [^] [pir_test]

--- pir_test/pir_test.ino
+++ pir_test/pir_test.ino
@@ -1,26 +1,18 @@
-//Henry's Bench
+// Based on "Henry's Bench"
 // HC-SR501 Motion Detector
-// Sample Sketch
 
-int ledPin = 13;  // LED on Pin 13 of Arduino
-int pirPin = 2; // Input for HC-S501
+int ledPin = 13;
+int pirPin = 2;
 
-int pirValue; // Place to store read PIR Value
-
-
-void setup() {
-  
+void setup()
+{
   pinMode(ledPin, OUTPUT);
   pinMode(pirPin, INPUT);
- 
+
   digitalWrite(ledPin, LOW);
-  
-  
-  
 }
 
-void loop() {
-  pirValue = digitalRead(pirPin);
-  digitalWrite(ledPin, pirValue);
-
+void loop()
+{
+  digitalWrite(ledPin, digitalRead(pirPin));
 }
