4136686967dceeb3548c8e1473653047baf40ac8
Ian <ian@somewhere.com>
2019-12-16 23:43:18 +0300

    [^+] [rtc_lcd] Added ability to manage thermostat.

--- rtc_lcd/rtc_lcd.ino
+++ rtc_lcd/rtc_lcd.ino
@@ -1,24 +1,30 @@
 // Table clock
 
 /*
-  Built on: 16x2 LCD display, DS3231 RTC, Arduino Uno
+  Built on: 16x2 LCD display, DS3231 RTC, relay module, Arduino Uno
 
   It sets DS3231 to emit square wave at 1Hz, connects output to
-  interrupt pin and use it to update display.
+  interrupt pin and use it to update display. Also it reads
+  temperature from DS3231 and if it below <DESIRED_TEMP_MIN> turns
+  <SWITCH_PIN> on. If it above <DESIRED_TEMP_MAX> - turns switch off.
 
   Wiring.
     Connect LCD to <LCD_..> pins.
     Connect DS3231.BBSQW to <TICK_PIN> pin which accepts interrupts.
+    Connect relay to <SWITCH_PIN>.
+    Adjust <DESIRED_TEMP_..> to required temperature band.
 */
 
 /*
   Status: stable, extending
   Last mod.: 2019-12-16
-  Version: 1.0.0
+  Version: 1.1.0
 */
 
 #include "me_ds3231.h"
-#include "DateTime.h"
+#include "me_switch.h"
+#include "me_DateTime.h"
+
 #include <LiquidCrystal.h>
 
 const uint8_t
@@ -29,18 +35,31 @@ const uint8_t
   LCD_D6 = 11,
   LCD_D7 = 12,
 
-  TICK_PIN = 3;
+  TICK_PIN = 3,
 
-LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+  SWITCH_PIN = 4;
+
+const float
+  DESIRED_TEMP_MIN = 24.0,
+  DESIRED_TEMP_MAX = 24.9;
+
+const uint8_t MAX_MSG_LEN = 2 * 16;
+char msg_buf[MAX_MSG_LEN];
+
 me_ds3231 ds3231 = me_ds3231();
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+c_switch thermostat = c_switch(SWITCH_PIN);
 
 void setup()
 {
-  lcd.begin(16, 2);
   Serial.begin(9600);
+  Serial.println("Hello.");
+
+  lcd.begin(16, 2);
 
   // SQW mode 0 - 1Hz square wave
   ds3231.setSqwMode(0);
+
   ds3231.disableSqwAtBattery();
 
   if (ds3231.wave32kEnabled())
@@ -63,14 +82,14 @@ void setup()
   pinMode(TICK_PIN, INPUT_PULLUP);
   uint8_t internal_pin_no = digitalPinToInterrupt(TICK_PIN);
   attachInterrupt(internal_pin_no, tick_handler, RISING);
-}
 
-const uint8_t MAX_MSG_LEN = 40;
-char msg_buf[MAX_MSG_LEN];
+  pinMode(SWITCH_PIN, OUTPUT);
+}
 
 void do_business()
 {
   DateTime dt = ds3231.getDateTime();
+  float temperature = ds3231.getTemp();
 
   dt.represent_date(msg_buf, MAX_MSG_LEN);
   lcd.setCursor(0, 0);
@@ -85,8 +104,19 @@ void do_business()
   lcd.print(msg_buf);
 
   lcd.setCursor(9, 1);
-  lcd.print(ds3231.getTemp(), 2);
+  lcd.print(temperature, 2);
   lcd.print("\337C");
+
+  if (thermostat.is_off() && (temperature < DESIRED_TEMP_MIN))
+  {
+    thermostat.switch_on();
+    Serial.println("Thermostat ON");
+  }
+  else if (thermostat.is_on() && (temperature > DESIRED_TEMP_MAX))
+  {
+    thermostat.switch_off();
+    Serial.println("Thermostat OFF");
+  }
 }
 
 volatile bool tick_registered;
