6e705ff2ea5f381f50501b70bc00c231610a43c6
Ian <ian@somewhere.com>
2020-02-17 07:13:37 +0300

    [*^] [lib.me_Thermostat]

--- libraries/me_Thermostat/me_Thermostat.h
+++ libraries/me_Thermostat/me_Thermostat.h
@@ -6,7 +6,7 @@ class cThermostat {
   public:
     float setpoint;
     float spread;
-    c_switch& relay;
+    c_switch relay;
     cThermostat(c_switch aRelay):
       setpoint(23),
       spread(1),
@@ -15,17 +15,31 @@ class cThermostat {
     virtual void process(float temperature);
 };
 
-class cHeatHoldingThermostat : cThermostat {
+class cHeatHoldingThermostat : public cThermostat {
   public:
     using cThermostat::cThermostat;
     void process(float temperature) {
+      /*
+      String msg = "";
+      msg =
+        msg +
+        "temperature: " + String(temperature, 2) +
+        " " +
+        "relay.is_off(): " + relay.is_off() +
+        " " +
+        "setpoint: " + String(setpoint, 2) +
+        " " +
+        "spread: " + String(spread, 2);
+
+      Serial.println(msg);
+      */
       if (relay.is_off() and (temperature < setpoint - spread / 2)) {
         relay.switch_on();
-        Serial.println("Thermostat ON");
+        // Serial.println("Thermostat ON");
       }
       if (relay.is_on() and (temperature > setpoint + spread / 2)) {
         relay.switch_off();
-        Serial.println("Thermostat OFF");
+        // Serial.println("Thermostat OFF");
       }
     };
 };
