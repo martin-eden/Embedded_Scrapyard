9ce497ddef2f7897cca3aa911c53e6987eca5581
Ian <ian@somewhere.com>
2020-03-02 03:52:06 +0300

    [^] [tests.4_digit_led_test] Code stylization.

--- tests/4_digit_led_test/4_digit_led_test.ino
+++ tests/4_digit_led_test/4_digit_led_test.ino
@@ -1,20 +1,29 @@
+// Test 4-digit LED by printing value from analog pin
+
+/*
+  Status: stable
+  Last mod.: 2020-03-02
+*/
+
 #include <TM1637Display.h>
 
 const uint8_t
-  input_pin = A4,
-  clock_pin = A5,
+  INPUT_PIN = A4,
+  CLOCK_PIN = A5,
 
-  analog_pin = A0;
+  ANALOG_PIN = A0;
 
-TM1637Display display(clock_pin, input_pin);
+const uint16_t
+  MEASUREMENT_DELAY_MS = 217;
 
-void setup()
-{
+TM1637Display display(CLOCK_PIN, INPUT_PIN);
+
+void setup() {
   display.setBrightness(0x0F);
 }
 
-void loop()
-{
-  uint16_t value = analogRead(analog_pin);
-  display.showNumberDec(value, true);
+void loop() {
+  uint16_t value = analogRead(ANALOG_PIN);
+  display.showNumberDec(value, false);
+  delay(MEASUREMENT_DELAY_MS);
 }
