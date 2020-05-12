8dcf8d15087ba7ec19133a6862ac40ba75e0b5d9
Ian <ian@somewhere.com>
2020-05-12 06:43:37 +0200

    [?+] [NeoPixel.Plasm] Added hue drift possibility.
    Hue drift not works, it sometimes capped at fixed value.
    
    Idea was to roll hue 0-255 while maintaining initial distance
    between start colors.

--- NeoPixel/Plasm/Plasm.ino
+++ NeoPixel/Plasm/Plasm.ino
@@ -9,10 +9,11 @@ const int16_t
   BRIGHTNESS = 64;
 
 const uint8_t
-  UPDATES_PER_SECOND = 180 / 60;
+  UPDATES_PER_SECOND = 180 / 60,
+  HUE_DRIFT = 2;
 
 const float
-  SCALE = 15.0 / (NUM_LEDS - LEDS_OFFSET - 1);
+  SCALE = 13.0 / (NUM_LEDS - LEDS_OFFSET - 1);
 
 CRGB leds[NUM_LEDS];
 
@@ -77,14 +78,33 @@ void FractalFill(
 
 uint32_t last_millis = millis();
 
+CRGB
+  start_color = CRGB::Chartreuse,
+  finish_color = CRGB::CornflowerBlue;
+
 void loop() {
   uint32_t cur_time = millis();
   uint32_t time_passed = cur_time - last_millis;
   last_millis = cur_time;
   // Serial.println(time_passed);
 
-  FractalFill(LEDS_OFFSET, NUM_LEDS - 1, CRGB::Chartreuse, CRGB::CornflowerBlue);
+  FractalFill(LEDS_OFFSET, NUM_LEDS - 1, start_color, finish_color);
 
   FastLED.show();
   FastLED.delay(1000 / UPDATES_PER_SECOND);
+
+  CHSV start_color_hsv = rgb2hsv_approximate(start_color);
+  CHSV finish_color_hsv = rgb2hsv_approximate(finish_color);
+
+  // start_color_hsv.hue += HUE_DRIFT;
+  // finish_color_hsv.hue += HUE_DRIFT;
+
+  start_color = start_color_hsv;
+  finish_color = finish_color_hsv;
+
+  /*
+  Serial.print(start_color_hsv.hue); Serial.print(" ");
+  Serial.print(finish_color_hsv.hue); Serial.print(" ");
+  Serial.println();
+  */
 }
