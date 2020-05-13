9e4be0e04447298ae6b08c8efd61d2ce1919a568
Ian <ian@somewhere.com>
2020-05-13 08:38:29 +0200

    [?^] [NeoPixel.Plasm] More or less implemented start colors drift.
    Still need to implement bouncing from value boundaries.
    
    Adding color fluctuations at high FPS creates flickering.
    At low FPS it looks random. No that trance effect as in glitched
    version with shadows.
    
    So ideally pixel colors in different generations of FractalFill()
    should be near.

--- NeoPixel/Plasm/Plasm.ino
+++ NeoPixel/Plasm/Plasm.ino
@@ -10,11 +10,15 @@ const int16_t
   BRIGHTNESS = 64;
 
 const uint8_t
-  UPDATES_PER_SECOND = 1,  // 180 / 60,
-  HUE_DRIFT = 0;
+  UPDATES_PER_SECOND = 30,  // 180 / 60,
+  BACKGROUND_NOISE = 0;
 
 const float
-  SCALE = 1.0;
+  SCALE = 0.0;
+
+float
+  START_HUE_DRIFT = PI / 3,
+  FINSH_HUE_DRIFT = -1.0 / 3;
 
 CRGB leds[NUM_LEDS];
 
@@ -35,12 +39,41 @@ void Hilight(
   if (leds[offset] == color) {
     // Serial.print("color already set at offset ");
     // Serial.println(offset);
-    return;
+    // return;
   } else {
     // Serial.println(offset);
   }
 
   leds[offset] = color;
+}
+
+int8_t GetMixNoise(
+  uint8_t noise_magnitude
+) {
+  int8_t result = random(-noise_magnitude / 2, noise_magnitude / 2);
+
+  return result;
+}
+
+int8_t GetHueNoise(
+  uint8_t noise_magnitude
+) {
+  int8_t result = random(-noise_magnitude / 2, noise_magnitude / 2);
+  result += random(-BACKGROUND_NOISE / 2, BACKGROUND_NOISE / 2);
+
+  return result;
+}
+
+CHSV MixColors(
+  CHSV start_color,
+  CHSV finish_color,
+  uint8_t noise_magnitude
+) {
+  fract8 mix_fract = 0x80 + GetMixNoise(noise_magnitude);
+  CHSV result = blend(start_color, finish_color, mix_fract);
+  result.hue += GetHueNoise(noise_magnitude);
+
+  return result;
 }
 
 void FractalFill(
@@ -66,12 +99,14 @@ void FractalFill(
   noise_magnitude = noise_magnitude * 0x100;
   noise_magnitude = constrain(noise_magnitude, 0, 0x100);
 
-  int8_t noise_value = random(-noise_magnitude / 2, noise_magnitude / 2);
-  // Serial.println(noise_value);
+  CHSV middle_color = MixColors(start_color, finish_color, noise_magnitude);
 
-  fract8 mix_fract = 0x80 - noise_value;
-
-  CHSV middle_color = blend(start_color, finish_color, mix_fract);
+  /*
+  Serial.print(middle_color.hue); Serial.print(" ");
+  Serial.print(middle_color.saturation); Serial.print(" ");
+  Serial.print(middle_color.value); Serial.print(" ");
+  Serial.println();
+  //*/
 
   /*
   Serial.print(start); Serial.print(" ");
@@ -94,11 +129,13 @@ void loop() {
   // Serial.println(time_passed);
 
   static CHSV
-    start_color = rgb2hsv_approximate(CRGB::Chartreuse),
-    finish_color = rgb2hsv_approximate(CRGB::CornflowerBlue);
+    start_color = CHSV(128, 255, 255),
+    // start_color = rgb2hsv_approximate(CRGB::Chartreuse),
+    finish_color = CHSV(0, 255, 255);
+    // finish_color = rgb2hsv_approximate(CRGB::CornflowerBlue);
 
   // .hue field values may be clamped. So we roll hue independently.
-  static uint8_t
+  static float
     start_color_hue = start_color.hue,
     finish_color_hue = finish_color.hue;
 
@@ -107,8 +144,21 @@ void loop() {
   FastLED.show();
   FastLED.delay(1000 / UPDATES_PER_SECOND);
 
-  start_color_hue += HUE_DRIFT;
-  finish_color_hue += HUE_DRIFT;
+  start_color_hue += START_HUE_DRIFT;
+  finish_color_hue += FINSH_HUE_DRIFT;
+
+  if (abs(start_color_hue - finish_color_hue) >= 0x70) {
+    /*
+    Serial.print(start_color_hue); Serial.print(" ");
+    Serial.print(finish_color_hue); Serial.print(" ");
+    Serial.println();
+    */
+
+    START_HUE_DRIFT = -START_HUE_DRIFT;
+    do {
+      start_color_hue += START_HUE_DRIFT;
+    } while (abs(start_color_hue - finish_color_hue) >= 0x70);
+  }
 
   start_color.hue = start_color_hue;
   finish_color.hue = finish_color_hue;
