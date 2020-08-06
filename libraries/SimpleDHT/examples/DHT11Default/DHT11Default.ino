383cbbecce5ee9d39ae76292e1215416aa499e2b
Ian <ian@somewhere.com>
2020-08-06 03:04:34 +0200

    [-] [lib.SimpleDHT.DHT11Default] Less verbose output.

--- libraries/SimpleDHT/examples/DHT11Default/DHT11Default.ino
+++ libraries/SimpleDHT/examples/DHT11Default/DHT11Default.ino
@@ -1,6 +1,6 @@
 #include <SimpleDHT.h>
 
-// for DHT11, 
+// for DHT11,
 //      VCC: 5V or 3V
 //      GND: GND
 //      DATA: 2
@@ -13,9 +13,9 @@ void setup() {
 
 void loop() {
   // start working...
-  Serial.println("=================================");
-  Serial.println("Sample DHT11...");
-  
+  // Serial.println("=================================");
+  // Serial.println("Sample DHT11...");
+
   // read without samples.
   byte temperature = 0;
   byte humidity = 0;
@@ -24,11 +24,11 @@ void loop() {
     Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
     return;
   }
-  
-  Serial.print("Sample OK: ");
-  Serial.print((int)temperature); Serial.print(" *C, "); 
+
+  // Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, ");
   Serial.print((int)humidity); Serial.println(" H");
-  
+
   // DHT11 sampling rate is 1HZ.
-  delay(1500);
+  delay(15000);
 }
