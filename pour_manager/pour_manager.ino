cf0f582bf7243fb9f2514c2e654777cca26970b1
Ian <ian@somewhere.com>
2020-11-16 19:27:50 +0300

    [^+] [pour_manager] Checks for possible sensor ranges.

--- pour_manager/pour_manager.ino
+++ pour_manager/pour_manager.ino
@@ -1,9 +1,13 @@
 // Measures soil dryness and pours if needed.
 
+const char
+  code_name[] = "Pour manager",
+  code_descr[] = "Measures soil dryness and pours if needed.",
+  version[] = "2.0.2";
+
 /*
   Status: stable
-  Generation: 0.8
-  Last mod.: 2020-11-09
+  Last mod.: 2020-11-16
 */
 
 /*
@@ -14,158 +18,61 @@
     * Water pump motor
 */
 
-#include <me_humidity_measurer.h>
 #include <me_switch.h>
-#include <me_DateTime.h>
 #include <TM1637Display.h>
-
-const char
-  code_descr[] = "\"Pour manager\". Based on \"Flower friend\" gardening system.",
-  version[] = "0.8";
 
 const uint8_t
   MEASURER_SIGNAL_PIN = A0,
-  MEASURER_POWER_PIN = 8,
+
   MOTOR_CONTROL_PIN = 2,
 
   DISPLAY_INPUT_PIN = 5,
   DISPLAY_CLOCK_PIN = 6;
 
-// Capacitive soil moisture sensor: (
-const uint8_t
-  DESIRED_RH_MIN = 60,
-  DESIRED_RH_MAX = 80;
-
 const uint16_t
-  MEASURER_MIN_VALUE = 80,
-  MEASURER_MAX_VALUE = 580,
-  MEASURER_HYSTERESIS = 10;
+  MEASURER_RANGE_LOW = 320,
+  MEASURER_RANGE_HIGH = 420,
+  MEASURER_ABS_LOW = 240,
+  MEASURER_ABS_HIGH = 650;
 
 const bool
   MEASURER_HIGH_MEANS_DRY = true;
-// )
 
 const uint32_t
-  IDLE_MEASUREMENT_DELAY = uint32_t(1000) * 7,
-  POUR_MEASUREMENT_DELAY = uint32_t(1000) * 5;
+  IDLE_MEASUREMENT_DELAY = uint32_t(1000) / 5, //uint32_t(1000) * 7,
+  POUR_MEASUREMENT_DELAY = uint32_t(1000) / 5; //uint32_t(1000) * 7;
 
-c_humidity_measurer measurer;
 c_switch motor = c_switch(MOTOR_CONTROL_PIN);
 TM1637Display display(DISPLAY_CLOCK_PIN, DISPLAY_INPUT_PIN);
-
-struct t_measurer_params
-  {
-    uint8_t power_pin;
-    uint8_t sensor_pin;
-    int16_t min_value;
-    int16_t max_value;
-    uint16_t hysteresis;
-    bool power_off_between_measures;
-    bool high_means_dry;
-  };
-
-const t_measurer_params sensor_params =
-  {
-    power_pin: MEASURER_POWER_PIN,
-    sensor_pin: MEASURER_SIGNAL_PIN,
-    min_value: MEASURER_MIN_VALUE,
-    max_value: MEASURER_MAX_VALUE,
-    hysteresis: MEASURER_HYSTERESIS,
-    power_off_between_measures: false,
-    high_means_dry: MEASURER_HIGH_MEANS_DRY
-  };
 
 void setup() {
   Serial.begin(9600);
 
   display.setBrightness(0x0F);
 
-  Serial.println("Setup. Motors.");
-  Serial.println("Setup. Sensors.");
-  init_moisture_sensors();
-
-  // assure normal business logic was done before printing status:
-  Serial.println("Setup. Loop.");
+  // Assure normal business logic was done before printing status:
   loop();
 
-  print_signature();
   print_usage();
   print_status();
 }
 
-void init_moisture_sensors() {
-  if (sensor_params.sensor_pin != -1) {
-    pinMode(sensor_params.sensor_pin, INPUT);
-    measurer.sensor_pin = sensor_params.sensor_pin;
-  }
-  if (sensor_params.min_value != -1)
-    measurer.min_value = sensor_params.min_value;
-  if (sensor_params.max_value != -1)
-    measurer.max_value = sensor_params.max_value;
-  measurer.hysteresis = sensor_params.hysteresis;
-  measurer.power_off_between_measures = sensor_params.power_off_between_measures;
-  measurer.high_means_dry = sensor_params.high_means_dry;
-  if (sensor_params.power_pin != -1) {
-    pinMode(sensor_params.power_pin, OUTPUT);
-    measurer.power_pin = sensor_params.power_pin;
-  }
-}
-
 uint32_t next_request_time;
 
-int parse_on_off(char c) {
-  if (c == '0')
-    return 0;
-  else if (c == '1')
-    return 1;
-  else
-    return -1;
-}
-
-void send_measurement(uint8_t value) {
-  String msg = "";
-  msg = msg + "value: " + value;
-  Serial.println(msg);
-}
-
-void print_signature() {
-  Serial.println("-----------------------------------");
-
-  Serial.print(code_descr);
-  Serial.println();
-
-  Serial.print("  Version: ");
-  Serial.print(version);
-  Serial.println();
-
-  Serial.print("  Uploaded: ");
-  Serial.print(get_upload_time());
-  Serial.println();
-
-  Serial.print("  File: ");
-  Serial.print(F(__FILE__));
-  Serial.println();
-
-  Serial.println("-----------------------------------");
-}
-
 const char
-  CMD_CLEAR_CLOCK_ERROR = 'C',
-  CMD_MEASURE = 'M',
-  CMD_MOTOR = 'T',
   CMD_GET_STATE = 'G';
 
 void print_usage() {
   String msg = "";
   msg =
     msg +
+    "----------------------------------------------------\n" +
+    "Code: \"" + code_name + "\"\n" +
+    "Description: " + code_descr + "\n" +
+    "Version: " + version + "\n" +
+    "----------------------------------------------------\n" +
     "Usage:" + "\n" +
-    "  " + CMD_MEASURE + "\n" +
-    "  " + "  " + "Measure probe." + "\n" +
-    "  " + CMD_MOTOR + " (0 | 1)" + "\n" +
-    "  " + "  " + "Enable/disable motor." + "\n" +
-    "  " + CMD_GET_STATE + "\n" +
-    "  " + "  " + "Print current status." + "\n" +
+    "  " + CMD_GET_STATE + " - Print current status.\n" +
     "\n";
   Serial.print(msg);
 }
@@ -176,22 +83,6 @@ void handle_command() {
   int8_t on_off;
   uint8_t value;
   switch (cmd) {
-    case CMD_MEASURE:
-      Serial.read();
-      get_humidity();
-      break;
-    case CMD_MOTOR:
-      if (data_length < 2)
-        break;
-      Serial.read();
-      on_off = parse_on_off(Serial.read());
-      if (on_off < 0)
-        break;
-      if (on_off == 1)
-        motor.switch_on();
-      else
-        motor.switch_off();
-      break;
     case CMD_GET_STATE:
       Serial.read();
       print_status();
@@ -203,47 +94,30 @@ void handle_command() {
   }
 }
 
-String pad_zeroes(uint8_t value) {
-  String result = "";
-  if (value < 10)
-    result = result + "0";
-  result = result + value;
-  return result;
+int16_t get_raw_value() {
+  return analogRead(MEASURER_SIGNAL_PIN);
 }
 
-String get_upload_time()
-{
-  String result = "";
-  DateTime upload_time = DateTime(F(__DATE__), F(__TIME__));
-  result =
-    result + upload_time.year() +
-    "-" + pad_zeroes(upload_time.month()) +
-    "-" + pad_zeroes(upload_time.day()) +
-    " " + pad_zeroes(upload_time.hour()) +
-    ":" + pad_zeroes(upload_time.minute()) +
-    ":" + pad_zeroes(upload_time.second()) +
-    "";
-  return result;
+bool is_line_problem() {
+  int16_t result = get_raw_value();
+  return (result <= MEASURER_ABS_LOW) || (result >= MEASURER_ABS_HIGH);
 }
 
-String represent_time_passed(uint32_t seconds) {
-  String result = "";
-  TimeSpan time_span = seconds;
-  bool construction_started = false;
+int16_t get_humidity() {
+  int16_t result = get_raw_value();
 
-  if (time_span.days() > 0) {
-    result = result + time_span.days() + "d ";
-    construction_started = true;
+  int16_t raw_result = result;
+
+  display.showNumberDec(result, false);
+
+  Serial.print("get_humidity: ");
+  Serial.println(result);
+  if (is_line_problem()) {
+    result = -1;
+    delay(500);
+    display.showNumberDec(result, false);
+    delay(500);
   }
-  if (construction_started || (time_span.hours() > 0)) {
-    result = result + time_span.hours() + "h ";
-    construction_started = true;
-  }
-  if (construction_started || (time_span.minutes() > 0)) {
-    result = result + time_span.minutes() + "m ";
-    construction_started = true;
-  }
-  result = result + time_span.seconds() + "s";
 
   return result;
 }
@@ -251,54 +125,37 @@ String represent_time_passed(uint32_t seconds) {
 uint32_t cur_time;
 
 void print_status() {
-  String msg;
+  String msg = "";
 
-  msg = "";
-  msg =
-    msg +
-    "Status:" + "\n" +
-    "  Pour settings:" + "\n" +
-    "    MEASURER_MIN_VALUE: " + MEASURER_MIN_VALUE + "\n" +
-    "    MEASURER_MAX_VALUE: " + MEASURER_MAX_VALUE + "\n" +
-    "    MEASURER_HYSTERESIS: " + MEASURER_HYSTERESIS + "\n" +
-    "    DESIRED_RH_MIN: " + DESIRED_RH_MIN + "\n" +
-    "    DESIRED_RH_MAX: " + DESIRED_RH_MAX + "\n";
-  Serial.print(msg);
-
-  Serial.print("    uptime: ");
-  Serial.print(represent_time_passed(cur_time / 1000));
-  Serial.print("\n");
-
-  Serial.println("  Delays:");
-
+  Serial.print("Status:\n");
+  Serial.print("  Delays:\n");
   Serial.print("    IDLE_MEASUREMENT_DELAY: ");
-  Serial.print((float)IDLE_MEASUREMENT_DELAY / 1000);
-  Serial.print("\n");
+  Serial.println((float)IDLE_MEASUREMENT_DELAY / 1000);
   Serial.print("    POUR_MEASUREMENT_DELAY: ");
-  Serial.print((float)POUR_MEASUREMENT_DELAY / 1000);
-  Serial.print("\n");
+  Serial.println((float)POUR_MEASUREMENT_DELAY / 1000);
 
-  Serial.println("  Block:");
-  uint8_t value = measurer.get_value();
-  uint16_t raw_value = measurer.get_raw_value();
-  uint8_t is_line_problem = measurer.is_line_problem; // (1)
+  Serial.print("Measurer:\n");
+
+  msg = "  Range: ";
+  msg =
+    msg +
+    "[" +  MEASURER_RANGE_LOW + ", " + MEASURER_RANGE_HIGH + "]\n";
+  Serial.print(msg);
+
+  Serial.print("  High values means dry?: ");
+  Serial.println(MEASURER_HIGH_MEANS_DRY);
+
+  Serial.print("  Humidity: ");
+  int16_t value = get_humidity();
+  Serial.println(value);
+
   msg = "";
   msg =
     msg +
-    "  " + "  " +
-    "moisture_humidity_%: " + value + ", " +
-    "raw_value: " + raw_value + ", " +
-    "is_line_problem: " + is_line_problem + ", " +
-    "motor: " + motor.is_on() + "\n";
+    "  Motor: " + motor.is_on() + "\n";
   Serial.print(msg);
 
   Serial.print("\n");
-  /*
-    [1]:
-      <is_line_problem> is set inside <get_value()>. In string
-      constructor "a() + b()" the actual call order is b(), a().
-      So we force correct order via direct assignments.
-  */
 }
 
 bool is_time_to_work() {
@@ -310,29 +167,18 @@ bool is_time_to_work() {
     );
 }
 
-int16_t get_humidity() {
-  int16_t result = measurer.get_value();
-  if (measurer.is_line_problem)
-    result = -1;
-
-  int16_t raw_result = measurer.get_raw_value();
-  display.showNumberDec(raw_result, false);
-  delay(1200);
-  display.showNumberDec(result, false);
-  Serial.print("get_humidity: ");
-  send_measurement(result);
-
-  return result;
-}
-
 void motor_on() {
-  // motor.switch_on();
-  // print_status();
+  if (motor.is_off()) {
+    motor.switch_on();
+    Serial.println("Motor ON.");
+  }
 }
 
 void motor_off() {
-  motor.switch_off();
-  print_status();
+  if (motor.is_on()) {
+    motor.switch_off();
+    Serial.println("Motor OFF.");
+  }
 }
 
 void update_next_request_time() {
@@ -349,14 +195,21 @@ void do_business() {
     return;
 
   int cur_hum = get_humidity();
-  if (cur_hum == -1)
-    motor_off();
-  else
-    if ((cur_hum <= DESIRED_RH_MIN) && motor.is_off())
+  if (cur_hum > MEASURER_RANGE_HIGH) {
+    if (MEASURER_HIGH_MEANS_DRY)
       motor_on();
     else
-      if ((cur_hum >= DESIRED_RH_MAX) && motor.is_on())
-        motor_off();
+      motor_off();
+  }
+  if (cur_hum < MEASURER_RANGE_LOW) {
+    if (MEASURER_HIGH_MEANS_DRY)
+      motor_off();
+    else
+      motor_on();
+  }
+
+  if (cur_hum == -1)
+    motor_off();
 
   update_next_request_time();
 }
@@ -375,4 +228,6 @@ void loop() {
   [...]
   2020-03-14
     Reduced mostly expreimental features of "Flower frined" to minimum.
+  2020-11-14
+    Reduced code even more.
 */
