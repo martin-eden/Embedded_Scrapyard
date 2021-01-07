19c500fd9c9f40d61b0c9f68de1846505e2b681c
Ian <ian@somewhere.com>
2021-01-07 23:47:18 +0300

    [/] [tests.test_something]

--- tests/test_something/test_something.ino
+++ tests/test_something/test_something.ino
@@ -1,36 +1,10 @@
-const int16_t HeatGranularity = 800;
-
-uint8_t GetNewHeat(const int16_t v) {
-  int16_t result = constrain(v, 0, 0xFF);
-  result = map(result, 0, 0xFF, 160, 0);
-  result = uint8_t(result);
-  return result;
-}
-
-uint8_t IncreaseHeat(uint8_t v) {
-  int16_t NewHeat = (int16_t)v + (int16_t)HeatGranularity;
-  NewHeat = GetNewHeat(NewHeat);
-  return NewHeat;
-}
-
-uint8_t DecreaseHeat(uint8_t v) {
-  Serial.print("v: ");
-  Serial.println(v);
-
-  int16_t NewHeat = (int16_t)v - (int16_t)HeatGranularity;
-  Serial.print("NewHeat: ");
-  Serial.println(NewHeat);
-
-  NewHeat = GetNewHeat(NewHeat);
-  Serial.print("NewHeat: ");
-  Serial.println(NewHeat);
-
-  return NewHeat;
-}
-
 void setup() {
   Serial.begin(9600);
-  DecreaseHeat(102);
+  uint32_t v1 = 0xFFFFFFFF, v2 = 0;
+  Serial.println(v2 - v1);
+  Serial.println((float)(v2 - v1));
+  Serial.println((float)v2 - v1);
+  Serial.println((float)(v2) - v1);
 }
 
 void loop() {
