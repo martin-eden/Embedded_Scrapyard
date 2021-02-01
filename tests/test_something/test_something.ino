372be3e33fd6ad3bab46268349b7349feab2a827
Ian <ian@somewhere.com>
2021-02-01 10:54:57 +0300

    [/] [tests.test_something]

--- tests/test_something/test_something.ino
+++ tests/test_something/test_something.ino
@@ -1,10 +1,15 @@
 void setup() {
   Serial.begin(9600);
-  uint32_t v1 = 0xFFFFFFFF, v2 = 0;
-  Serial.println(v2 - v1);
-  Serial.println((float)(v2 - v1));
-  Serial.println((float)v2 - v1);
-  Serial.println((float)(v2) - v1);
+  const uint32_t DESIRED_POUR_CYCLE_H = 48;
+  float numHoursPassed = 52.1;
+  float correctionCoeff = DESIRED_POUR_CYCLE_H / numHoursPassed;
+  uint32_t pour_duration_ms = 120000;
+  pour_duration_ms = pour_duration_ms * correctionCoeff;
+  Serial.println(DESIRED_POUR_CYCLE_H);
+  Serial.println(numHoursPassed);
+  Serial.println(correctionCoeff);
+  Serial.println(pour_duration_ms);
+
 }
 
 void loop() {
