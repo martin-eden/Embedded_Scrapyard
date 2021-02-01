e99003bd931729d762ec8bcd6d9f8c9e70751010
Ian <ian@somewhere.com>
2021-02-01 12:09:59 +0300

    [/] [tests.test_something]

--- tests/test_something/test_something.ino
+++ tests/test_something/test_something.ino
@@ -1,14 +1,12 @@
 void setup() {
   Serial.begin(9600);
-  const uint32_t DESIRED_POUR_CYCLE_H = 48;
-  float numHoursPassed = 52.1;
-  float correctionCoeff = DESIRED_POUR_CYCLE_H / numHoursPassed;
-  uint32_t pour_duration_ms = 120000;
-  pour_duration_ms = pour_duration_ms * correctionCoeff;
-  Serial.println(DESIRED_POUR_CYCLE_H);
-  Serial.println(numHoursPassed);
-  Serial.println(correctionCoeff);
-  Serial.println(pour_duration_ms);
+
+  const float temperature = 23.497;
+  const int32_t temperature_rounded = (temperature * 10);
+
+  Serial.println(temperature);
+  Serial.println(temperature_rounded);
+  Serial.println(int32_t(temperature * 10));
 
 }
 
