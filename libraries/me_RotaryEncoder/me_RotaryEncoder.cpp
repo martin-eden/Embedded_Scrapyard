f3df1bef046bf76576b9785fd33584f1839c38d7
Ian <ian@somewhere.com>
2021-04-28 23:48:33 +0200

    [+>] [lib.me_RotaryEncoder]

---
+++ libraries/me_RotaryEncoder/me_RotaryEncoder.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include "me_RotaryEncoder.h"
+
+RotaryEncoder::RotaryEncoder(uint8_t _PhaseA_pin, uint8_t _PhaseB_pin, uint8_t _Switch_pin) {
+  PhaseA_pin = _PhaseA_pin;
+  PhaseB_pin = _PhaseB_pin;
+  Switch_pin = _Switch_pin;
+
+  pinMode(PhaseA_pin, INPUT_PULLUP);
+  pinMode(PhaseB_pin, INPUT_PULLUP);
+  pinMode(Switch_pin, INPUT_PULLUP);
+
+  CurState = GetPinsState();
+  UpdateState();
+  PositionHasChanged = true;
+
+  SwitchState = GetSwitchState();
+  LastSwitchTime_ms = 0;
+  UpdateSwitch();
+  SwitchHasChanged = true;
+}
+
+uint8_t RotaryEncoder::GetState(bool PhaseA, bool PhaseB) {
+  if (!PhaseA & !PhaseB)
+    return 0;
+  else if (!PhaseA & PhaseB)
+    return 1;
+  else if (PhaseA & PhaseB)
+    return 2;
+  else if (PhaseA & !PhaseB)
+    return 3;
+  else
+    return 100;
+}
+
+uint8_t RotaryEncoder::GetPinsState() {
+  return GetState(digitalRead(PhaseA_pin), digitalRead(PhaseB_pin));
+}
+
+const int8_t UnreachableState = 50;
+
+int8_t StateDelta[4][4] = {
+  {0, 1, UnreachableState, -1},
+  {-1, 0, 1, UnreachableState},
+  {UnreachableState, -1, 0, 1},
+  {1, UnreachableState, -1, 0}
+};
+
+void RotaryEncoder::UpdateState() {
+  PrevState = CurState;
+  CurState = GetPinsState();
+
+  int8_t StateDelta_value = StateDelta[CurState][PrevState];
+  if (StateDelta_value == UnreachableState)
+    CurState = PrevState;
+  else
+    Position += StateDelta_value;
+
+  if (CurState != PrevState)
+    PositionHasChanged = true;
+}
+
+bool RotaryEncoder::GetSwitchState() {
+  return digitalRead(Switch_pin);
+}
+
+void RotaryEncoder::UpdateSwitch() {
+  PrevSwitchState = SwitchState;
+  SwitchState = GetSwitchState();
+  int32_t CurTime_ms = millis();
+  if (SwitchState != PrevSwitchState) {
+    if (CurTime_ms - LastSwitchTime_ms > Debounce_ms) {
+      LastSwitchTime_ms = CurTime_ms;
+      SwitchHasChanged = true;
+    }
+  }
+}
---
+++ libraries/me_RotaryEncoder/me_RotaryEncoder.h
@@ -0,0 +1,76 @@
+// Rotary encoder tracking
+
+/*
+  Status: done
+  Version: 1.0
+  Last mod.: 2021-04-28
+*/
+
+/*
+  Rotary encoder model: KY-040
+
+  Tracking done via hardware interrupts on two phase pins. For button press
+  software polling is used, as there are just two hardware interrupt pins
+  available on Arduino Uno. Button press event is filtered by debouncing.
+
+  Wiring:
+
+    CLK (PhaseA_pin) => 2 or 3
+    DT (PhaseB_pin) => 3 or 2
+    SW (Switch_pin) => any pin
+
+  Usage:
+
+    RotaryEncoder RotaryEncoder(PhaseA_pin, PhaseB_pin, Switch_pin);
+    RotaryEncoder.Debounce_ms = Debounce_ms;
+    attachInterrupt(digitalPinToInterrupt(PhaseA_pin), Phase_change, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PhaseB_pin), Phase_change, CHANGE);
+
+    void Phase_change() {
+      RotaryEncoder.UpdateState();
+    }
+
+    cli();
+    int32_t Position_copy = RotaryEncoder.Position;
+    sei();
+    if (RotaryEncoder.PositionHasChanged) {
+      Process(Position_copy);
+      ...
+      RotaryEncoder.PositionHasChanged = false;
+    }
+
+    RotaryEncoder.UpdateSwitch();
+    if (RotaryEncoder.SwitchHasChanged) {
+      Process(RotaryEncoder.SwitchState);
+      ...
+      RotaryEncoder.SwitchHasChanged = false;
+    }
+*/
+
+#pragma once
+
+class RotaryEncoder {
+  public:
+    RotaryEncoder(uint8_t _PhaseA_pin, uint8_t _PhaseB_pin, uint8_t _Switch_pin);
+    uint8_t PhaseA_pin;
+    uint8_t PhaseB_pin;
+    uint8_t Switch_pin;
+    uint32_t Debounce_ms = 40;
+
+    int32_t Position = 0;
+    bool PositionHasChanged = false;
+
+    bool SwitchState;
+    bool SwitchHasChanged = false;
+
+    void UpdateState();
+    void UpdateSwitch();
+  private:
+    uint8_t CurState, PrevState;
+    bool PrevSwitchState;
+    int32_t LastSwitchTime_ms;
+
+    uint8_t GetState(bool PhaseA, bool PhaseB);
+    uint8_t GetPinsState();
+    bool GetSwitchState();
+};
