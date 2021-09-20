7b2045b365f7fbf80f55c4789bfd75cfdc316064
Ian <ian@somewhere.com>
2021-09-19 20:45:15 -0400

    [^] [tests.RotaryEncoder.test_oled]

--- tests/RotaryEncoder/test_oled/test_oled.ino
+++ tests/RotaryEncoder/test_oled/test_oled.ino
@@ -2,8 +2,8 @@
 
 /*
   Status: done
-  Version: 1.0
-  Last mod.: 2021-07-18
+  Version: 2.0
+  Last mod.: 2021-09-19
 */
 
 #include <me_RotaryEncoder.h>
@@ -21,99 +21,144 @@ const uint8_t
 const int32_t
   Debounce_ms = 40;
 
-RotaryEncoder RotaryEncoder(PhaseA_pin, PhaseB_pin, Switch_pin);
-Adafruit_ST7789 Display = Adafruit_ST7789(CS_pin, DC_pin, RST_pin);
+Adafruit_ST7789 display = Adafruit_ST7789(CS_pin, DC_pin, RST_pin);
+
+class EncoderGauge {
+public:
+  EncoderGauge();
+  int32_t getNumPositions();
+  void setNumPositions(int32_t _numPositions);
+  void setPosition(int32_t _position);
+  void initDisplay();
+private:
+  int32_t numPositions;
+  int32_t position;
+  int32_t prevPosition;
+  void erasePointer();
+  void drawPointer();
+  void drawPointer_raw(int32_t _position, int16_t _color);
+  void drawText_raw(int32_t _position, int16_t _color);
+  void drawBackground();
+};
+
+EncoderGauge::EncoderGauge() {
+  numPositions = 80;
+}
+
+void EncoderGauge::initDisplay() {
+  display.init(240, 240);
+  display.setRotation(2);
+  display.fillScreen(ST77XX_BLACK);
+  display.setTextSize(2);
+
+  drawBackground();
+
+  prevPosition = numPositions;
+  position = 0;
+  setPosition(position);
+}
+
+int32_t EncoderGauge::getNumPositions() {
+  return numPositions;
+}
+
+void EncoderGauge::setNumPositions(int32_t _numPositions) {
+  if (_numPositions != 0) {
+    erasePointer();
+    numPositions = _numPositions;
+    drawPointer();
+  }
+}
+
+void EncoderGauge::setPosition(int32_t _position) {
+  if (_position == prevPosition)
+    return;
+
+  position = _position;
+
+  erasePointer();
+  drawPointer();
+
+  prevPosition = position;
+}
+
+void EncoderGauge::erasePointer() {
+  drawPointer_raw(prevPosition, ST77XX_BLACK);
+  drawText_raw(prevPosition, ST77XX_BLACK);
+}
+
+void EncoderGauge::drawPointer() {
+  drawPointer_raw(position, ST77XX_YELLOW);
+  drawText_raw(position, ST77XX_WHITE);
+}
+
+void EncoderGauge::drawBackground() {
+  uint16_t x, y, radius, color;
+  x = display.width() / 2;
+  y = display.height() / 2;
+
+  radius = 120;
+  color = ST77XX_BLUE;
+/*  for (uint8_t i = 0; i < 6; ++i) {
+    display.drawCircle(x, y, radius - i, color);
+  }
+*/
+  display.fillCircle(x, y, radius, color);
+}
+
+void EncoderGauge::drawPointer_raw(int32_t _position, int16_t _color) {
+  float scale = 2 * PI / numPositions;
+  float circleX, circleY;
+  uint16_t radius = 6;
+  circleX = 120 + (104 + radius) * sin(_position * scale);
+  circleY = 120 - (104 + radius) * cos(_position * scale);
+
+  display.fillCircle(circleX, circleY, radius, _color);
+}
+
+void EncoderGauge::drawText_raw(int32_t _position, int16_t _color) {
+  const uint16_t
+    x = 0,
+    y = 0;
+
+  display.setTextColor(_color);
+  display.setCursor(x, y);
+  display.print(_position);
+}
+
+EncoderGauge encoderGauge;
+RotaryEncoder rotaryEncoder(PhaseA_pin, PhaseB_pin, Switch_pin);
 
 void setup() {
   Serial.begin(9600);
 
-  RotaryEncoder.Debounce_ms = Debounce_ms;
+  rotaryEncoder.Debounce_ms = Debounce_ms;
 
   attachInterrupt(digitalPinToInterrupt(PhaseA_pin), Phase_change, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PhaseB_pin), Phase_change, CHANGE);
-  
-  initDisplay();  
+
+  encoderGauge.initDisplay();
 }
 
 void Phase_change() {
-  RotaryEncoder.UpdateState();
+  rotaryEncoder.UpdateState();
 }
-
-bool PrevSwitchState_printed = false;
 
 void loop() {
-  int32_t Position = RotaryEncoder.GetPosition();
-    
-  drawState(Position);
-  RotaryEncoder.PositionHasChanged = false;
+  int32_t position = rotaryEncoder.GetPosition();
+  rotaryEncoder.PositionHasChanged = false;
 
-  RotaryEncoder.UpdateSwitch();
-  if (RotaryEncoder.SwitchHasChanged) {
-    bool NeedSwitchRedisplay = (RotaryEncoder.SwitchState != PrevSwitchState_printed);
-    if (NeedSwitchRedisplay) {
-      Serial.print("Switch ");
-      Serial.println(RotaryEncoder.SwitchState);
-      PrevSwitchState_printed = RotaryEncoder.SwitchState;
+  encoderGauge.setPosition(position);
+
+  rotaryEncoder.UpdateSwitch();
+  if (rotaryEncoder.SwitchHasChanged) {
+    rotaryEncoder.SwitchHasChanged = false;
+    // Do something only after switch was pressed and released:
+    if (!rotaryEncoder.SwitchState) {
+      if (position != 0.0) {
+        encoderGauge.setNumPositions(position);
+        Serial.println(encoderGauge.getNumPositions());
+      }
     }
-    RotaryEncoder.SwitchHasChanged = false;
   }
-}
-
-void initDisplay() {
-  Display.init(240, 240);
-  Display.setRotation(2);
-  Display.fillScreen(ST77XX_BLACK);
-  Display.setTextSize(2);
-  drawBackground();
-}
-
-void drawBackground() {
-  uint16_t x, y, radius, color;
-  x = Display.width() / 2;
-  y = Display.height() / 2;
-  
-  radius = 120;
-  color = ST77XX_BLUE;
-  for (uint8_t i = 0; i < 4; ++i) {
-    Display.drawCircle(x, y, radius - i, color);
-  }
-}
-
-void drawState(int32_t Position) {
-  static int32_t PrevPosition = -1;
-  if (Position == PrevPosition)
-    return; 
- 
-  /*  
-  Serial.print("Position ");
-  Serial.println(Position);
-  */
-  
-  const uint16_t 
-    x = 120 - 4,
-    y = 120 - 4;
-    
-  Display.setTextColor(ST77XX_BLACK);
-  Display.setCursor(x, y);
-  Display.print(PrevPosition);
-
-  Display.setTextColor(ST77XX_WHITE);
-  Display.setCursor(x, y);
-  Display.print(Position);
-
-  drawPointer(PrevPosition, ST77XX_BLACK);    
-  drawPointer(Position, ST77XX_YELLOW);    
-
-  PrevPosition = Position;
-}
-
-void drawPointer(int32_t position, int16_t color) {
-  static float scale = 2 * PI / 80.0;
-  float circleX, circleY;
-  uint16_t radius = 12;
-  circleX = 120 + 100 * sin(position * scale);
-  circleY = 120 - 100 * cos(position * scale);
-  
-  Display.fillCircle(circleX, circleY, radius, color); 
-
 }
