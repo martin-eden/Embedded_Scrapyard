399e5c2adf0911575aabfd1e7f17d9c1b1ba9d28
Ian <ian@somewhere.com>
2022-02-12 16:08:53 -0500

    [^~] [hygrometer_128x32]

--- hygrometer_128x32/hygrometer_128x32.ino
+++ hygrometer_128x32/hygrometer_128x32.ino
@@ -2,8 +2,8 @@
 
 /*
   Status: stable
-  Version: 1.2
-  Last mod.: 2022-02-11
+  Version: 1.4
+  Last mod.: 2022-02-12
 */
 
 /*
@@ -14,17 +14,15 @@
 */
 
 #include <me_DHT11.h>
-#include <SimpleDHT.h>
 #include <U8g2lib.h>
 
 const uint8_t
   Hygrometer_pin = 12;
 
 const float
-  MeasurementDelay_sec = 2.5;
+  MeasurementDelay_sec = 1.0;
 
 me_DHT11 Hygrometer(Hygrometer_pin);
-//SimpleDHT11 Hygrometer(Hygrometer_pin);
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C Display(U8G2_R0);
 
 void setup()
@@ -34,60 +32,55 @@ void setup()
   Display.setFont(u8g2_font_lubR14_tf);
 }
 
-void fillScreen()
+void PrintRawData()
 {
+  String DataStr = String(Hygrometer.Data[0], HEX) + " ";
+  DataStr += String(Hygrometer.Data[1], HEX) + " ";
+  DataStr += String(Hygrometer.Data[2], HEX) + " ";
+  DataStr += String(Hygrometer.Data[3], HEX) + " ";
+  DataStr += String(Hygrometer.Data[4], HEX) + " ";
+  Serial.println(DataStr);
+}
+
+void DisplayString(String aStr)
+{
+  char buf[20];
+  aStr.toCharArray(buf, 20);
+
+  uint8_t x = 8;
+  uint8_t y = 28;
+
   Display.clearBuffer();
-  for (uint8_t x = 0; x < 128; ++x)
-  {
-    for (uint8_t y = 0; y < 32; ++y)
-    {
-      if ((32 * y + x) % 3 == 0)
-        Display.drawPixel(x, y);
-    }
-  }
+  Display.drawStr(x, y, buf);
   Display.sendBuffer();
 }
 
 void loop()
 {
   float
-    hygrometerTemperature,
-    hygrometerHumidity;
+    hygrometerHumidity,
+    hygrometerTemperature;
 
-  int16_t result = Hygrometer.Read(&hygrometerHumidity, &hygrometerTemperature);
-  //int16_t result = Hygrometer.read2(&hygrometerTemperature, &hygrometerHumidity, NULL);
-  if (result != me_DHT11_OK)
-  //if (result != SimpleDHTErrSuccess)
+  bool result = Hygrometer.Get();
+  if (result)
+  {
+    hygrometerHumidity = Hygrometer.Humidity;
+    hygrometerTemperature = Hygrometer.Temperature;
+  }
+  else
   {
     hygrometerHumidity = -1.0;
     hygrometerTemperature = -128.0;
   }
 
-  String value_str = String(hygrometerHumidity, 0) + "%";
-  value_str += " " + String(hygrometerTemperature, 1) + "\260C";
+  String valueStr = String(hygrometerHumidity, 0) + "%";
+  valueStr += " " + String(hygrometerTemperature, 1) + "\260C";
 
-  // Serial.println(value_str);
+  // Serial.println(valueStr);
 
-  /*
-  delay(3000);
-  uint16_t hygrometerHumidityRaw, hygrometerTemperatureRaw;
-  uint8_t resultRaw = Hygrometer.ReadRaw(&hygrometerHumidityRaw, &hygrometerTemperatureRaw);
-  Serial.print(hygrometerHumidityRaw, HEX);
-  Serial.print(" ");
-  Serial.print(hygrometerTemperatureRaw, HEX);
-  Serial.print("\n");
-  */
+  DisplayString(valueStr);
 
-
-  char buf_str[20];
-  value_str.toCharArray(buf_str, 20);
-
-  uint8_t x = 8;
-  uint8_t y = 28;
-
-  Display.clearBuffer();
-  Display.drawStr(x, y, buf_str);
-  Display.sendBuffer();
+  PrintRawData();
 
   delay(MeasurementDelay_sec * 1000);
 }
