2f05dcbdb531a71965c53eefe0722b29636d9c88
Ian <ian@somewhere.com>
2022-02-18 12:54:53 -0500

    [?/] [tests.oled.128x32] Current messy state of 128x32 OLED test.

--- tests/oled/128x32/128x32.ino
+++ tests/oled/128x32/128x32.ino
@@ -4,34 +4,66 @@ U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C Display(U8G2_R0);
 
 void setup()
 {
+  // Serial.begin(9600);
   Display.begin();
+  Display.setFont(u8g2_font_lubR24_tn);
+  Display.setFontMode(1);
 }
 
-void fillScreen()
+void fillScreen(int16_t fillRatio)
 {
-  Display.clearBuffer();
-  for (uint8_t x = 0; x < 128; ++x)
+  int16_t fillCounter = 0;
+
+  for (uint8_t y = 0; y < 32; ++y)
   {
-    for (uint8_t y = 0; y < 32; ++y)
+    for (uint8_t x = 0; x < 128; ++x)
     {
-      if ((32 * y + x) % 3 == 0)
-        Display.drawPixel(x, y);
+      /*
+      fillCounter += fillRatio;
+      // fillCounter += random(-fillRatio / 5, fillRatio / 5);
+      if (fillCounter >= 1023)
+      {
+        fillCounter -= 1023;
+        Display.setDrawColor(1);
+      }
+      else
+        Display.setDrawColor(0);
+      */
+      if (random(1024) <= fillRatio)
+        Display.setDrawColor(1);
+      else
+        Display.setDrawColor(0);
+
+      Display.drawPixel(x, y);
     }
   }
-  Display.sendBuffer();
 }
 
-void clearScreen()
+void displayString(String aStr)
 {
-  Display.clearBuffer();
-  Display.drawBox(0, 0, 128, 32);
-  Display.sendBuffer();
+  char buf[20];
+  aStr.toCharArray(buf, 20);
+
+  uint8_t x = 28;
+  uint8_t y = 28;
+
+  Display.setDrawColor(2);
+  Display.drawStr(x, y, buf);
 }
 
 void loop()
 {
-  fillScreen();
-  delay(2000);
-  // clearScreen();
-  // delay(1500);
+  int16_t lighting = 1023 - analogRead(A0);
+
+  // Serial.println(lightingStr);
+
+  // for (uint16_t lighting = 0; lighting < 1024; ++lighting)
+  {
+    Display.clearBuffer();
+    fillScreen(lighting);
+    // String lightingStr = String(lighting);
+    // displayString(lightingStr);
+    Display.sendBuffer();
+    delay(7000);
+  }
 }
