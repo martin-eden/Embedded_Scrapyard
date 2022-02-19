fb0fa49db2f40cf07615b6718c04518e0d809cdc
Ian <ian@somewhere.com>
2022-02-19 17:04:16 -0500

    [~] [128x32_LightMeter]

--- demo/128x32_LightMeter/128x32_LightMeter.ino
+++ demo/128x32_LightMeter/128x32_LightMeter.ino
@@ -1,41 +1,26 @@
 #include <U8g2lib.h>
 #include <me_ProgressBar.h>
+#include <me_RandomDots.h>
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C Display(U8G2_R0);
-me_ProgressBar ProgressBar((U8G2*) &Display, 10, 7, 120, 25);
+me_ProgressBar ProgressBar((U8G2*) &Display, 8, 6, 119, 25);
+me_RandomDots RandomDots((U8G2*) &Display, 0, 0, 127, 31);
 
 void setup()
 {
-  // Serial.begin(9600);
   Display.begin();
-}
-
-void fillScreen(uint16_t fillRatio)
-{
-  for (uint8_t y = 0; y < 32; ++y)
-  {
-    for (uint8_t x = 0; x < 128; ++x)
-    {
-      if ((fillRatio != 0) && (random(1024) <= fillRatio))
-        Display.setDrawColor(1);
-      else
-        Display.setDrawColor(0);
-
-      Display.drawPixel(x, y);
-    }
-  }
 }
 
 void loop()
 {
   uint16_t lighting = 1023 - analogRead(A0);
-  // Serial.println(lightingStr);
+
+  RandomDots.CurrentFillRatio = lighting;
   ProgressBar.CurrentValue = lighting;
 
   Display.clearBuffer();
 
-  fillScreen(lighting);
-
+  RandomDots.Draw();
   ProgressBar.Draw();
 
   Display.sendBuffer();
