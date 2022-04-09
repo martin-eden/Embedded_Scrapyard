8fc42ba88c3b9050cbf9b2ac6bbf9bef9c877c5e
Ian <ian@somewhere.com>
2022-04-09 17:29:17 -0400

    [+] [tests.IrNecParser.128x32]

---
+++ tests/IrNecParser/128x32/128x32.ino
@@ -0,0 +1,60 @@
+// Capturing and parsing IR signals in NEC format.
+
+/*
+  Status: stable
+  Version: 1.1
+  Last mod.: 2022-04-02
+*/
+
+#include <me_DigitalSignalRecorder.h>
+#include <me_IrNecParser.h>
+#include <me_StatePrinters_IrNecParser_128x32.h>
+#include <U8g2lib.h>
+
+using namespace IrNecParser;
+
+me_DigitalSignalRecorder DSR;
+me_IrNecParser IrDecoder(&DSR);
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C Display(U8G2_R2);
+
+const uint8_t
+  SignalPin = 2; // 2 or 3 for ATmega328P
+
+const uint16_t
+  RecorderCapacity = 36;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("IR NEC protocol decoder test.\nDisplays results on 128x32 OLED display.");
+
+  Display.begin();
+  Display.setContrast(10);
+
+  if (!DSR.SetCapacity(RecorderCapacity))
+  {
+    Serial.println("Not enough memory for recorder's buffer.");
+    delay(1000);
+    exit(1);
+  }
+
+  pinMode(SignalPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(SignalPin), OnSignalChange, CHANGE);
+}
+
+void OnSignalChange()
+{
+  DSR.Add(micros(), digitalRead(SignalPin));
+}
+
+void loop()
+{
+  if (IrDecoder.Get())
+  {
+    Display.clearBuffer();
+    IrNec_DisplayState(&IrDecoder, &Display);
+    Display.sendBuffer();
+  }
+
+  delay(100);
+}
