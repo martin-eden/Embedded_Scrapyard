b79df987a846eca3b036916da04ecd13b3bcea44
Ian <ian@somewhere.com>
2022-04-16 16:04:51 -0400

    [!/] [me_StatePrinters_IrNecParser_128x32] New display layout.

--- libraries/me_StatePrinters_IrNecParser_128x32/me_StatePrinters_IrNecParser_128x32.cpp
+++ libraries/me_StatePrinters_IrNecParser_128x32/me_StatePrinters_IrNecParser_128x32.cpp
@@ -2,8 +2,8 @@
 
 /*
   Status: stable
-  Version: 1.0
-  Last mod.: 2022-04-09
+  Version: 1.1
+  Last mod.: 2022-04-16
 */
 
 #include "me_StatePrinters_IrNecParser_128x32.h"
@@ -11,20 +11,28 @@
 #include <me_IrNecParser.h>
 #include <U8g2lib.h>
 
+void DisplayGridLines(U8G2* Display)
+{
+  Display->drawVLine(66, 0, 32);
+  Display->drawVLine(106, 0, 32);
+}
+
 void DisplayAddress(U8G2* Display, uint16_t Address)
 {
-  char Buffer[7];
-  Display->setFont(u8g2_font_amstrad_cpc_extended_8r);
-  sprintf(Buffer, "0x%04X", Address);
-  Display->drawStr(0, 8, Buffer);
+  char Buffer[5];
+  sprintf(Buffer, "%04X", Address);
+
+  Display->setFont(u8g2_font_logisoso24_tr);
+  Display->drawStr(0, 26, Buffer);
 }
 
 void DisplayCommand(U8G2* Display, uint8_t Command)
 {
-  char Buffer[5];
-  Display->setFont(u8g2_font_chargen_92_tr);
-  sprintf(Buffer, "0x%02X", Command);
-  Display->drawStr(62, 26, Buffer);
+  char Buffer[3];
+  sprintf(Buffer, "%02X", Command);
+
+  Display->setFont(u8g2_font_logisoso24_tr);
+  Display->drawStr(71, 26, Buffer);
 }
 
 void DisplayHasShortRepeat(U8G2* Display, bool HasShortRepeat)
@@ -32,7 +40,7 @@ void DisplayHasShortRepeat(U8G2* Display, bool HasShortRepeat)
   if (HasShortRepeat)
   {
     Display->setFont(u8g2_font_chargen_92_tr);
-    Display->drawGlyph(109, 26, '.');
+    Display->drawGlyph(107, 27, '.');
   }
 }
 
@@ -40,14 +48,15 @@ void DisplayIsRepeat(U8G2* Display, bool IsRepeat)
 {
   if (IsRepeat)
   {
-    Display->setFont(u8g2_font_unifont_t_86);
     const uint16_t ReplaySymbolCode = 0x2b6e;
-    Display->drawGlyph(42, 25, ReplaySymbolCode);
+    Display->setFont(u8g2_font_unifont_t_86);
+    Display->drawGlyph(113, 18, ReplaySymbolCode);
   }
 }
 
 void IrNec_DisplayState(IrNecParser::me_IrNecParser* IrNec, U8G2* Display)
 {
+  DisplayGridLines(Display);
   DisplayAddress(Display, IrNec->Address);
   DisplayCommand(Display, IrNec->Command);
   DisplayHasShortRepeat(Display, IrNec->HasShortRepeat);
