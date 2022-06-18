8b39870d7258328c3ed60b1368706ec2158adabf
Ian <ian@somewhere.com>
2022-06-18 19:11:38 -0400

    [+] [me_DHT11_StatePrinter_128x32]

---
+++ libraries/me_DHT11_StatePrinter_128x32/me_DHT11_StatePrinter_128x32.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#include <me_DHT11_StateGetter.h>
+
+namespace me_DHT11_StatePrinter_128x32
+{
+  class StatePrinter
+  {
+    public:
+      StatePrinter(U8G2* aDisplay) : Display(aDisplay) {};
+
+      void Print(me_DHT11_StateGetter::State state);
+
+    private:
+      U8G2* Display;
+
+      void DisplayOuterFrame();
+      void DisplayGridLines();
+      void DisplayTemperature(float Temperature);
+      void DisplayHumidity(float Humidity);
+      void DisplayFlipFlop();
+      void DisplayReadError();
+  };
+}
