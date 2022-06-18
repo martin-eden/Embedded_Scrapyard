8cdd9853b38d0f58fc9ec96b05fada39e49dde61
Ian <ian@somewhere.com>
2022-06-18 19:12:23 -0400

    [^/] [hygrometer_128x32] Adopted to new code structure.

--- hygrometer_128x32/hygrometer_128x32.ino
+++ hygrometer_128x32/hygrometer_128x32.ino
@@ -2,8 +2,8 @@
 
 /*
   Status: stable
-  Version: 2.0
-  Last mod.: 2022-06-17
+  Version: 3.0
+  Last mod.: 2022-06-18
 */
 
 /*
@@ -13,121 +13,38 @@
     Hygrometer: DHT11, <Hygrometer_pin>
 */
 
-#include <me_DHT11.h>
 #include <U8g2lib.h>
+
+#include <me_DHT11.h>
+#include <me_DHT11_StateGetter.h>
+#include <me_DHT11_StatePrinter_128x32.h>
 
 const uint8_t
   Hygrometer_pin = 2;
 
-const float
-  MeasurementDelay_sec = 1.0;
+const uint32_t
+  MeasurementDelay_ms = 6000;
 
-me_DHT11 Hygrometer(Hygrometer_pin);
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C Display(U8G2_R0);
+
+me_DHT11::DHT11 Hygrometer(Hygrometer_pin);
+me_DHT11_StatePrinter_128x32::StatePrinter StatePrinter(&Display);
 
 void setup()
 {
-  Serial.begin(57600);
   Display.begin();
-}
-
-void DisplayOuterFrame()
-{
-  const u8g2_uint_t
-    OuterFrameWidgetX = 0,
-    OuterFrameWidgetY = 0,
-    OuterFrameWidgetWidth = 128,
-    OuterFrameWidgetHeight = 32;
-
-  Display.drawFrame(
-    OuterFrameWidgetX,
-    OuterFrameWidgetY,
-    OuterFrameWidgetWidth,
-    OuterFrameWidgetHeight
-  );
-}
-
-void DisplayGridLines()
-{
-  const u8g2_uint_t
-    GridLinesWidgetX1 = 80,
-    GridLinesWidgetX2 = 123;
-
-  Display.drawVLine(GridLinesWidgetX1, 4, 25);
-  Display.drawVLine(GridLinesWidgetX2, 4, 25);
-}
-
-void DisplayTemperature(float Temperature)
-{
-  const u8g2_uint_t
-    TemperatureWidgetX = 5,
-    TemperatureWidgetY = 23;
-
-  char Buffer[8];
-  String(String(Temperature, 1) + "\260C").toCharArray(Buffer, sizeof(Buffer));
-
-  Display.setFont(u8g2_font_profont22_tf);
-  Display.drawStr(TemperatureWidgetX, TemperatureWidgetY, Buffer);
-}
-
-void DisplayHumidity(float Humidity)
-{
-  const u8g2_uint_t
-    HumidityWidgetX = 85,
-    HumidityWidgetY = 23;
-
-  char Buffer[8];
-  String(String(Humidity, 0) + "%") .toCharArray(Buffer, sizeof(Buffer));
-
-  Display.setFont(u8g2_font_profont22_tf);
-  Display.drawStr(HumidityWidgetX, HumidityWidgetY, Buffer);
-}
-
-void DisplayFlipFlop()
-{
-  const u8g2_uint_t
-    FlipFlopWidgetX = 125,
-    FlipFlopWidgetY = 8,
-    FlipFlopWidgetHeight = 19;
-
-  static bool flip = false;
-  if (flip)
-  {
-    // Display.drawVLine(FlipFlopWidgetX, FlipFlopWidgetY, FlipFlopWidgetHeight);
-  }
-
-  flip = !flip;
-}
-
-void DisplayState()
-{
-  DisplayOuterFrame();
-  DisplayGridLines();
-  DisplayTemperature(Hygrometer.Temperature);
-  DisplayHumidity(Hygrometer.Humidity);
-  DisplayFlipFlop();
-}
-
-String GetRawData()
-{
-  String Result = String(Hygrometer.Data[0], HEX) + " ";
-  Result += String(Hygrometer.Data[1], HEX) + " ";
-  Result += String(Hygrometer.Data[2], HEX) + " ";
-  Result += String(Hygrometer.Data[3], HEX) + " ";
-  Result += String(Hygrometer.Data[4], HEX) + " ";
-
-  return Result;
 }
 
 void loop()
 {
+  me_DHT11_StateGetter::State DataState;
+
+  Hygrometer.Request();
+  DataState = me_DHT11_StateGetter::GetState(&Hygrometer);
+
   Display.clearBuffer();
-
-  Hygrometer.Get();
-
-  DisplayState();
-
+  StatePrinter.Print(DataState);
   Display.sendBuffer();
 
-  delay(MeasurementDelay_sec * 1000);
+  delay(MeasurementDelay_ms);
 }
