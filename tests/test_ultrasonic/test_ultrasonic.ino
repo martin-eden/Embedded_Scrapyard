1476d988db5d7de266960c5fa21034c953fe786d
Ian <ian@somewhere.com>
2022-06-20 21:20:47 -0400

    [.^] [test.test_ultrasonic]
    Will eventually remove this old code.

--- tests/test_ultrasonic/test_ultrasonic.ino
+++ tests/test_ultrasonic/test_ultrasonic.ino
@@ -4,58 +4,51 @@
 */
 
 /*
-  Status: done
-  Version: 1.0
-  Last mod.: 2020-01-06
+  Status: stable
+  Version: 1.1
+  Last mod.: 2022-06-19
 */
 
 const uint8_t
-  TRIGGER_PIN = 11,
-  ECHO_PIN = 12;
-
-const uint8_t
-  BURST_DURATION_MCR = 10;
+  TRIGGER_PIN = 6,
+  ECHO_PIN = 7;
 
 const uint16_t
-  INTERPULSE_DELAY_MS = 330;
+  PULSE_DURATION_MCR = 100,
+  MEASUREMENT_DELAY_MS = 1000;
 
 const float
-  SPEED_OF_SOUND_MPS = 340, // meters per second
-  CM_PER_MCR = SPEED_OF_SOUND_MPS * 100 / 1000000;
-
+  SPEED_OF_SOUND_MPS = 340.0,
+  MM_PER_MCR = SPEED_OF_SOUND_MPS * 1E3 * 1E-6;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(57600);
 
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-
-  Serial.print("speed of sound: centimeters per microsecond: ");
-  Serial.println(CM_PER_MCR, 6);
 }
 
 void loop()
 {
-  uint16_t duration_mcr, cm;
+  uint32_t duration_mcr, mm;
 
   digitalWrite(TRIGGER_PIN, HIGH);
-  delayMicroseconds(BURST_DURATION_MCR);
+  delayMicroseconds(PULSE_DURATION_MCR);
   digitalWrite(TRIGGER_PIN, LOW);
 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  duration_mcr = pulseIn(ECHO_PIN, HIGH);
+  duration_mcr = pulseInLong(ECHO_PIN, HIGH, 30000);
 
-  // Convert the time into a distance
-  cm = (duration_mcr / 2) * CM_PER_MCR;
+  if (duration_mcr > 0)
+  {
+    mm = (duration_mcr / 2) * MM_PER_MCR;
 
-  Serial.print(duration_mcr);
-  Serial.print("mcr ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
+    // Serial.print(duration_mcr);
+    // Serial.print("mcr ");
+    Serial.print(mm);
+    // Serial.print("cm");
+    Serial.println();
+  }
 
-  delay(INTERPULSE_DELAY_MS);
+  delay(MEASUREMENT_DELAY_MS);
 }
