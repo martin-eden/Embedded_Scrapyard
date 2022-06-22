e3493adb52da53de7abdadec2f57f06a4dbcbb1c
Ian <ian@somewhere.com>
2022-06-22 00:07:29 -0400

    [~] [tests.SR04] Added OLED 128x32 display.

--- tests/SR04/SR04.ino
+++ tests/SR04/SR04.ino
@@ -8,6 +8,8 @@
 
 #include <me_SR04.h>
 #include <me_SR04_StateGetter.h>
+#include <me_SR04_StatePrinter_128x32.h>
+#include <U8g2lib.h>
 
 const uint8_t
   TriggerPin = 4,
@@ -16,20 +18,32 @@ const uint8_t
 const uint32_t
   MeasurementDelayMs = 60;
 
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C Display(U8G2_R2);
+
 me_SR04::SR04 Sonar(TriggerPin, EchoPin);
+me_SR04_StatePrinter_128x32::StatePrinter StatePrinter(&Display);
 
 void setup()
 {
   Serial.begin(57600);
+
+  Display.begin();
+  Display.setContrast(10);
 }
 
 void loop()
 {
   Sonar.Request();
   me_SR04_StateGetter::State RequestStatus = me_SR04_StateGetter::GetState(&Sonar);
+
+  Display.clearBuffer();
+  StatePrinter.Print(RequestStatus);
+  Display.sendBuffer();
+
   if (RequestStatus.HasDistance)
   {
     Serial.println(RequestStatus.DistanceCm);
   }
+
   delay(MeasurementDelayMs);
 }
