2e937e453b2629b03d142be38a88a8834f328cf1
Ian <ian@somewhere.com>
2022-07-07 03:39:50 -0400

    [/] [me_CapacitiveFilter]

--- libraries/me_CapacitiveFilter/me_CapacitiveFilter.h
+++ libraries/me_CapacitiveFilter/me_CapacitiveFilter.h
@@ -1,15 +1,23 @@
 #pragma once
 
+/*
+  Status: stable
+  Version: 1.0
+  Last mod.: 2022-07-07
+*/
+
 class CapacitiveFilter {
   public:
     CapacitiveFilter(float a_maxAccumulatedError);
-    void addValue(float value);
-    float getValue();
+    void Add(float value);
+    float Get();
+    bool ValueHasChanged();
   private:
     float maxAccumulatedError;
     float accumulatedError;
     uint32_t stepsDone;
     float baseValue;
+    bool baseValueIsChanged;
 };
 
 CapacitiveFilter::CapacitiveFilter(float a_maxAccumulatedError) {
@@ -17,21 +25,30 @@ CapacitiveFilter::CapacitiveFilter(float a_maxAccumulatedError) {
   accumulatedError = 0;
   stepsDone = 0;
   baseValue = 0;
+  baseValueIsChanged = false;
 }
 
-void CapacitiveFilter::addValue(float value) {
+void CapacitiveFilter::Add(float value) {
   float error = value - baseValue;
 
   accumulatedError += error;
 
   ++stepsDone;
   if (abs(accumulatedError) > maxAccumulatedError) {
-    baseValue += (accumulatedError / stepsDone);
+    float errorCorrection = accumulatedError / stepsDone;
+    baseValueIsChanged = true;
+    baseValue += errorCorrection;
     accumulatedError = 0;
     stepsDone = 0;
   }
+  else
+    baseValueIsChanged = false;
 }
 
-float CapacitiveFilter::getValue() {
+float CapacitiveFilter::Get() {
   return baseValue;
+}
+
+bool CapacitiveFilter::ValueHasChanged() {
+  return baseValueIsChanged;
 }
