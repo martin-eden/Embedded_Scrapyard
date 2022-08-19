15fc144fb8b2090fa4d13900f547d3ce731e8836
martin-eden <martin-eden@users.noreply.github.com>
2022-08-19 12:06:59 -0400

    [^] [rpm_counter]

--- rpm_counter/rpm_counter.ino
+++ rpm_counter/rpm_counter.ino
@@ -1,30 +1,29 @@
-// Approximating rpm by counting peaks via ISR
+// Counting by ISP
 
 /*
-  Status: ok
-  Last mod.: 2022-08-13
+  Status: stable
+  Last mod.: 2022-08-19
+*/
+
+/*
+  Used to count rotations of DC motor.
+
+  (Small magnet was attached to wheel and Hall sensor was used.)
 */
 
 #include <U8g2lib.h>
 
-const uint32_t measure_delay_ms = 60000;
+const uint32_t
+  MeasurementsIntervalMs = 60000;
 
-const uint8_t sensor_pin = 2; // hard-wired for Uno, other values won't work
-const uint8_t interrupt_number = 0; // depends on <sensor_pin>
-
-volatile uint32_t rpm_counter = 0;
+const uint8_t
+  SensorPin = 2; // should support hardware interrupts, pin 2 or 3 for Uno
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C Display(U8G2_R2);
 
-void pin_handler()
-{
-  rpm_counter++;
-}
-
 void setup()
 {
-  pinMode(sensor_pin, INPUT);
-  attachInterrupt(interrupt_number, pin_handler, FALLING);
+  pinMode(SensorPin, INPUT);
 
   Serial.begin(57600);
 
@@ -33,22 +32,49 @@ void setup()
   Display.setFont(u8g2_font_profont22_tf);
 }
 
-void loop()
+uint32_t Counter = 0;
+
+void ConnectCounter()
 {
-  Serial.print(rpm_counter);
+  attachInterrupt(digitalPinToInterrupt(SensorPin), CounterISP, FALLING);
+}
+
+void DisconnectCounter()
+{
+  detachInterrupt(digitalPinToInterrupt(SensorPin));
+}
+
+void CounterISP()
+{
+  Counter++;
+}
+
+void SendState()
+{
+  Serial.print(Counter);
   Serial.print("\n");
+}
 
-  Display.clearBuffer();
-
+void DisplayState()
+{
   const u8g2_uint_t
     WidgetX = 18,
     WidgetY = 23;
 
-  char Buffer[8];
-  String(String(rpm_counter)).toCharArray(Buffer, sizeof(Buffer));
+  Display.clearBuffer();
+
+  char Buffer[10];
+  String(String(Counter)).toCharArray(Buffer, sizeof(Buffer));
   Display.drawStr(WidgetX, WidgetY, Buffer);
 
   Display.sendBuffer();
+}
 
-  delay(measure_delay_ms);
+void loop()
+{
+  DisconnectCounter();
+  SendState();
+  DisplayState();
+  ConnectCounter();
+  delay(MeasurementsIntervalMs);
 }
