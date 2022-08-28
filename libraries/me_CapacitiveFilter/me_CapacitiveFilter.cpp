ac68e11b04ece54a8964e85219db8fa59a158e3b
Ian <ian@somewhere.com>
2022-08-28 12:56:14 -0400

    [+] [me_CapacitiveFilter]

--- libraries/me_CapacitiveFilter/me_CapacitiveFilter.cpp
+++ libraries/me_CapacitiveFilter/me_CapacitiveFilter.cpp
@@ -1,40 +1,82 @@
 /*
   Status: stable
-  Version: 1.0
-  Last mod.: 2022-07-07
+  Version: 1.1
+  Last mod.: 2022-08-28
 */
 
 #include "me_CapacitiveFilter.h"
 
-CapacitiveFilter::CapacitiveFilter(float a_maxAccumulatedError) {
-  maxAccumulatedError = a_maxAccumulatedError;
+CapacitiveFilter::CapacitiveFilter(float a_capacitance, uint32_t a_autoDischargeSpan)
+{
   accumulatedError = 0;
   stepsDone = 0;
   baseValue = 0;
   baseValueIsChanged = false;
+  SetCapacitance(a_capacitance);
+  SetAutoDischargeSpan(a_autoDischargeSpan);
 }
 
-void CapacitiveFilter::Add(float value) {
+void CapacitiveFilter::Add(float value)
+{
   float error = value - baseValue;
 
   accumulatedError += error;
 
   ++stepsDone;
-  if (abs(accumulatedError) > maxAccumulatedError) {
-    float errorCorrection = accumulatedError / stepsDone;
-    baseValueIsChanged = true;
-    baseValue += errorCorrection;
-    accumulatedError = 0;
-    stepsDone = 0;
-  }
+
+  if (stepsDone >= autoDischargeSpan)
+    Flush();
+
+  if (abs(accumulatedError) > capacitance)
+    Flush();
   else
     baseValueIsChanged = false;
 }
 
-float CapacitiveFilter::Get() {
+float CapacitiveFilter::Get()
+{
   return baseValue;
 }
 
-bool CapacitiveFilter::ValueHasChanged() {
+void CapacitiveFilter::Flush()
+{
+  if (stepsDone == 0)
+    return;
+
+  float errorCorrection = accumulatedError / stepsDone;
+  baseValueIsChanged = true;
+  baseValue += errorCorrection;
+  accumulatedError = 0;
+  stepsDone = 0;
+}
+
+bool CapacitiveFilter::ValueHasChanged()
+{
   return baseValueIsChanged;
 }
+
+void CapacitiveFilter::SetCapacitance(float a_capacitance)
+{
+  capacitance = a_capacitance;
+}
+
+float CapacitiveFilter::GetCapacitance()
+{
+  return capacitance;
+}
+
+float CapacitiveFilter::GetAccumulatedError()
+{
+  return accumulatedError;
+}
+
+void CapacitiveFilter::SetAutoDischargeSpan(uint32_t a_autoDischargeSpan)
+{
+  autoDischargeSpan = a_autoDischargeSpan;
+}
+
+uint32_t CapacitiveFilter::GetAutoDischargeSpan()
+{
+  return autoDischargeSpan;
+}
+
--- libraries/me_CapacitiveFilter/me_CapacitiveFilter.h
+++ libraries/me_CapacitiveFilter/me_CapacitiveFilter.h
@@ -2,16 +2,28 @@
 
 #include <Arduino.h>
 
-class CapacitiveFilter {
+class CapacitiveFilter
+{
   public:
-    CapacitiveFilter(float a_maxAccumulatedError);
+    CapacitiveFilter(float a_capacitance, uint32_t a_autoDischargeSpan = 100);
     void Add(float value);
+    void Flush();
     float Get();
     bool ValueHasChanged();
+
+    void SetCapacitance(float a_capacitance);
+    float GetCapacitance();
+
+    float GetAccumulatedError();
+
+    void SetAutoDischargeSpan(uint32_t a_autoDischargeSpan);
+    uint32_t GetAutoDischargeSpan();
+
   private:
-    float maxAccumulatedError;
+    float capacitance;
     float accumulatedError;
     uint32_t stepsDone;
     float baseValue;
     bool baseValueIsChanged;
+    uint32_t autoDischargeSpan;
 };
