aadff80c06104d77f0be6f66221679904728b224
Ian <ian@somewhere.com>
2022-08-31 21:31:56 -0400

    [/^] [SR04] Current state of SR04 distance meter sketch.

--- tests/SR04/SR04.ino
+++ tests/SR04/SR04.ino
@@ -11,9 +11,8 @@
 #include <me_SR04_StatePrinter_128x32.h>
 #include <me_CapacitiveFilter.h>
 #include <me_Statistics_TimePoint.h>
-#include <me_Statistics_Min.h>
-#include <me_Statistics_Max.h>
 #include <me_Statistics.Bucket.h>
+#include <me_Stat_AverageFilter.h>
 
 #include <U8g2lib.h>
 
@@ -37,9 +36,12 @@ U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C Display(U8G2_R2);
 me_SR04::SR04 Sonar(TriggerPin, EchoPin);
 CapacitiveFilter dataSmoother(DataFilterCapacitance, DataFilterAutoFlushSteps);
 me_SR04_StatePrinter_128x32::StatePrinter StatePrinter(&Display);
-me_Statistics_Min::MinValue minValue(me_Statistics_TimePoint::TimePoint(1000.0, 0));
-me_Statistics_Max::MaxValue maxValue(me_Statistics_TimePoint::TimePoint(0.0, 0));
-me_Statistics_Bucket::Bucket Bucket;
+
+me_Statistics_TimePoint::TimePoint DefaultMinValue(1000.0, 0);
+me_Statistics_TimePoint::TimePoint DefaultMaxValue(0.0, 0);
+
+me_Statistics_Bucket::Bucket Bucket(0.0);
+me_Stat_AverageFilter::Filter Averager(1000);
 
 void setup()
 {
@@ -55,12 +57,62 @@ float EstimateJitter(float Distance)
   float result;
 
   const float
-    MinJitter = 1.2,
+    MinJitter = 1.1,
     JitterPerDistance = 0.014;
 
   result = max(MinJitter, Distance * JitterPerDistance);
 
   return result;
+}
+
+float FloatMap(float Value, float SrcRangeMin, float SrcRangeMax, float DestRangeMin, float DestRangeMax)
+{
+  if (
+    (SrcRangeMin >= SrcRangeMax) ||
+    (DestRangeMin >= DestRangeMax)
+  )
+    return -1.0;
+
+  Value = max(Value, SrcRangeMin);
+  Value = min(Value, SrcRangeMax);
+
+  float SrcOffset = Value - SrcRangeMin;
+  float SrcSpan = SrcRangeMax - SrcRangeMin;
+  float SrcPos = SrcOffset / SrcSpan;
+  float DestPos = SrcPos;
+  float DestSpan = DestRangeMax - DestRangeMin;
+  float DestOffset = DestPos * DestSpan;
+  float Result = DestOffset + DestRangeMin;
+
+  return Result;
+}
+
+void DisplayFineMark(
+  U8G2* Display,
+  me_Statistics_Bucket::Bucket* Bucket,
+  float DistanceCm
+)
+{
+  const u8g2_uint_t
+    WidgetX = 2,
+    WidgetY = 2,
+    WidgetWidth = 124,
+    WidgetHeight = 28,
+    WidgetToothHeight = 4;
+
+  u8g2_uint_t X =
+    FloatMap(
+      DistanceCm,
+      Bucket->GetMinValue(),
+      Bucket->GetMaxValue(),
+      WidgetX,
+      WidgetX + WidgetWidth
+    );
+
+  // Display->drawVLine(X, WidgetY, WidgetHeight);
+
+  Display->drawVLine(X, WidgetY, WidgetToothHeight);
+  Display->drawVLine(X, WidgetY + WidgetHeight - WidgetToothHeight, WidgetToothHeight);
 }
 
 void loop()
@@ -74,8 +126,6 @@ void loop()
   if (RequestStatus.HasDistance)
   {
     using namespace me_Statistics_TimePoint;
-    using namespace me_Statistics_Min;
-    using namespace me_Statistics_Max;
 
     static int32_t TimePosition = 0;
 
@@ -84,40 +134,29 @@ void loop()
     dataSmoother.SetCapacitance(sensorDistance * CapacitancePerDistance);
     dataSmoother.Add(sensorDistance);
 
-    float smoothedDistance = dataSmoother.Get();
-
-    minValue.Add(TimePoint(smoothedDistance, TimePosition));
-
-    maxValue.Add(TimePoint(smoothedDistance, TimePosition));
-
-    float kt = 0.25;
-    if (!minValue.ValueHasChanged())
-    {
-      float k = pow(smoothedDistance / minValue.Get().Value, kt);
-      minValue = MinValue(TimePoint(minValue.Get().Value * k, TimePosition));
-    }
-
-    if (!maxValue.ValueHasChanged())
-    {
-      float k = pow(smoothedDistance / maxValue.Get().Value, kt);
-      maxValue = MaxValue(TimePoint(maxValue.Get().Value * k, TimePosition));
-    }
+    Averager.Add(sensorDistance);
 
     if (!Bucket.Add(sensorDistance))
     {
-      float Jitter = EstimateJitter(smoothedDistance);
+      float Jitter = EstimateJitter(sensorDistance);
       Serial.print("Jitter = ");
       Serial.println(Jitter);
 
       Bucket.Reset(Jitter);
       Bucket.Add(sensorDistance);
+
       dataSmoother.Flush();
+      dataSmoother.Add(sensorDistance);
+
+      Averager.Reset();
+      Averager.Add(sensorDistance);
+
       Serial.println("Bucket recreated.");
     }
 
     ++TimePosition;
 
-    RequestStatus.DistanceCm = dataSmoother.Get();
+    RequestStatus.DistanceCm = Averager.Get();
 
     Serial.print(sensorDistance);
 
@@ -125,22 +164,22 @@ void loop()
     Serial.print(dataSmoother.Get());
 
     Serial.print(" ");
-    Serial.print(dataSmoother.GetAccumulatedError());
+    Serial.print(Averager.Get());
 
     Serial.print(" ");
-    Serial.print(Bucket.GetCurrentSpan());
+    Serial.print(Bucket.GetMaxSpan());
 
-    // Serial.print(" ");
-    // Serial.print(minValue.Get().Value);
+    Serial.print(" ");
+    Serial.print(Bucket.GetMinValue());
 
-    // Serial.print(" ");
-    // Serial.print(maxValue.Get().Value);
+    Serial.print(" ");
+    Serial.print(Bucket.GetMaxValue());
 
     Serial.println();
-
   }
 
   Display.clearBuffer();
+  DisplayFineMark(&Display, &Bucket, RequestStatus.DistanceCm);
   StatePrinter.Print(RequestStatus);
   Display.sendBuffer();
 
