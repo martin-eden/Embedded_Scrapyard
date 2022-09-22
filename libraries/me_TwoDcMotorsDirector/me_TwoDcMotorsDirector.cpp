630154e619f2ed65f104498980b4dc4aae864a72
Ian <ian@somewhere.com>
2022-09-21 22:42:43 -0400

    [!+] [me_TwoMotorsDirector]

---
+++ libraries/me_TwoDcMotorsDirector/me_TwoDcMotorsDirector.cpp
@@ -0,0 +1,66 @@
+#include "me_TwoDcMotorsDirector.h"
+
+TwoDcMotorsDirector::TwoDcMotorsDirector(DcMotor *aLeftMotor, DcMotor *aRightMotor)
+{
+  LeftMotor = aLeftMotor;
+  RightMotor = aRightMotor;
+
+  SetDirection(0);
+  SetPower(0);
+}
+
+void TwoDcMotorsDirector::SetDirection(int16_t aDirection)
+{
+  // Official direction is angle in degrees [low(int16_t), high(int16_t)].
+  OfficialDirection = aDirection;
+
+  // Internal direction is angle in degrees [0, 359]:
+  DirectionDeg = aDirection % 360;
+  if (DirectionDeg < 0)
+    DirectionDeg += 360;
+
+  Actualize();
+}
+
+int16_t TwoDcMotorsDirector::GetDirection()
+{
+  return OfficialDirection;
+}
+
+void TwoDcMotorsDirector::SetPower(uint8_t aPower)
+{
+  // Official power is byte [0, 255]:
+  Power = aPower;
+
+  Actualize();
+}
+
+float sign(float a)
+{
+  if (a < 0)
+    return -1.0;
+  else if (a > 0)
+    return 1.0;
+  else
+    return 0.0;
+}
+
+void TwoDcMotorsDirector::Actualize()
+{
+  float CosinedDirection = (float) cos(DEG_TO_RAD * DirectionDeg); // [0, 1]
+  float SinedDirection = (float) sin(DEG_TO_RAD * DirectionDeg); // [0, 1]
+
+  float Lateral = sq(CosinedDirection) * Power; // [0, Power]
+  float Longitudinal = sq(SinedDirection) * Power; // [0, Power]
+  // ^ Assert: Longitudinal + Lateral == Power
+
+  float LeftMotorPower, RightMotorPower;
+  LeftMotorPower = Longitudinal * sign(SinedDirection) + Lateral * sign(CosinedDirection);
+  RightMotorPower =  Longitudinal * sign(SinedDirection) - Lateral * sign(CosinedDirection);
+
+  LeftMotor->SetIsBackward((LeftMotorPower < 0));
+  LeftMotor->SetPower((uint8_t) abs(LeftMotorPower));
+
+  RightMotor->SetIsBackward((RightMotorPower < 0));
+  RightMotor->SetPower((uint8_t) abs(RightMotorPower));
+}
---
+++ libraries/me_TwoDcMotorsDirector/me_TwoDcMotorsDirector.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Arduino.h>
+#include <me_DcMotor.h>
+
+class TwoDcMotorsDirector
+{
+  public:
+    TwoDcMotorsDirector(DcMotor *LeftMotor, DcMotor *RightMotor);
+
+    void SetDirection(int16_t Direction); // direction in degrees
+    int16_t GetDirection();
+    void SetPower(uint8_t Power); // power [0, 255]
+    uint8_t GetPower();
+
+  private:
+    DcMotor *LeftMotor, *RightMotor;
+
+    int16_t OfficialDirection;
+    int16_t DirectionDeg;
+    uint8_t Power;
+
+    void Actualize();
+};
