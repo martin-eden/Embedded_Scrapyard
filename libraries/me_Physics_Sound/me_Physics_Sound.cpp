a2672853d10b7b09d9609b7df1869770ed353b2f
Ian <ian@somewhere.com>
2022-11-01 20:14:32 -0400

    [^] [me_Physics_Sound]

--- libraries/me_Physics_Sound/me_Physics_Sound.cpp
+++ libraries/me_Physics_Sound/me_Physics_Sound.cpp
@@ -1,16 +1,19 @@
 // Functions to calculate sound properties.
 
 /*
-  Status: first
-  Last mod.: 2022-06-21
-  Version: 1.0
+  Status: stable
+  Last mod.: 2022-11-01
+  Version: 1.1
 */
 
 #include "me_Physics_Sound.h"
 
 float me_Physics_Sound::GetDistanceFromEchoCm(uint32_t EchoDelayMcr)
 {
-  const float SoundSpeedMpS = 340.0;
-  float distanceCm = EchoDelayMcr * 1e-6 * SoundSpeedMpS * 1e2 / 2;
+  // https://en.wikipedia.org/wiki/Speed_of_sound
+  const float SoundSpeedMpS = 343.0;
+
+  float distanceCm = ((EchoDelayMcr / 2) * 1e-6) * SoundSpeedMpS * 1e2;
+
   return distanceCm;
 }
--- libraries/me_Physics_Sound/me_Physics_Sound.h
+++ libraries/me_Physics_Sound/me_Physics_Sound.h
@@ -1,5 +1,23 @@
 // Functions to calculate sound properties.
 
+/*
+  Status: stable
+  Last mod.: 2022-11-01
+  Version: 1.0
+*/
+
+/*
+  Usage
+
+    float GetDistanceFromEchoCm(uint32_t EchoDelayMcr)
+
+      Calculate distance from echo delay.
+
+      * Assumed signal medium is air at 20 deg C.
+      * Echo delay in microseconds.
+      * Result in centimeters.
+*/
+
 #pragma once
 
 #include <Arduino.h>
