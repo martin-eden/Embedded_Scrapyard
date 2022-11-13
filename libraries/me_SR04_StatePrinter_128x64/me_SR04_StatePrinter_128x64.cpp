31c98b33d61d901a6cce9958c5bfdcff2dae5bb7
Ian <ian@somewhere.com>
2022-11-13 13:07:27 -0500

    [^] [me_SR04_StatePrinter_128x32] DisplayDistance() improvement.

--- libraries/me_SR04_StatePrinter_128x64/me_SR04_StatePrinter_128x64.cpp
+++ libraries/me_SR04_StatePrinter_128x64/me_SR04_StatePrinter_128x64.cpp
@@ -2,8 +2,8 @@
 
 /*
   Status: stable
-  Version: 1.0
-  Last mod.: 2022-11-07
+  Version: 1.1
+  Last mod.: 2022-11-12
 */
 
 #include "me_SR04_StatePrinter_128x64.h"
@@ -22,32 +22,51 @@ StatePrinter::StatePrinter(U8G2* aScreen)
 void StatePrinter::DisplayDistance(float DistanceCm)
 {
   const uint8_t BufferSize = 16;
+
+  uint16_t TextX;
+  uint16_t TextY;
+  uint16_t TextWidth;
+  uint16_t TextHeight;
+
+  uint16_t UnderscoreWidth;
+  const uint16_t UnderscoreHeight = 2;
+  uint16_t UnderscoreX;
+  uint16_t UnderscoreY;
+  const uint16_t UnderscoreOffsetY = 4;
+  const uint16_t UnderscoreOverdrawX = 2;
+
+  uint16_t DistanceInt;
+  uint8_t DistanceFrac;
+
   char Buffer[BufferSize];
 
-  uint16_t DistanceInt = DistanceCm;
-  uint8_t DistanceFrac = uint32_t(DistanceCm * 10) % 10;
+  DistanceInt = DistanceCm;
+  DistanceFrac = uint32_t(DistanceCm * 10) % 10;
+
   snprintf(Buffer, BufferSize, "%d.%d", DistanceInt, DistanceFrac);
 
-  uint16_t NumericLineWidth = Screen->getStrWidth(Buffer);
+  UnderscoreWidth = Screen->getStrWidth(Buffer);
 
   strcat(Buffer, " cm");
 
-  uint16_t TextWidth = Screen->getStrWidth(Buffer);
-  uint16_t TextHeight = Screen->getAscent() + Screen->getDescent();
+  TextWidth = Screen->getStrWidth(Buffer);
+  TextHeight = Screen->getAscent() + Screen->getDescent();
 
-  uint16_t TextX = GetCenterX() - (TextWidth / 2);
-  uint16_t TextY = GetCenterY() + (TextHeight / 2);
+  TextX = GetCenterX() - (TextWidth / 2);
+  TextY = GetCenterY() + (TextHeight / 2);
 
-  const uint16_t LineOffsetY = 4;
-  const uint16_t LineOverdrawX = 2;
+  UnderscoreWidth += UnderscoreOverdrawX * 2;
+  UnderscoreX = TextX - UnderscoreOverdrawX;
+  UnderscoreY = TextY + UnderscoreOffsetY;
 
-  NumericLineWidth += LineOverdrawX * 2;
-  uint16_t NumericLineX = TextX - LineOverdrawX;
-  uint16_t NumericLineY = TextY + LineOffsetY;
+  TextX = MapX(TextX);
+  TextY = MapY(TextY);
+  UnderscoreX = MapX(UnderscoreX);
+  UnderscoreY = MapY(UnderscoreY);
 
   Screen->setFont(u8g2_font_profont22_tf);
-  Screen->drawStr(MapX(TextX), MapY(TextY), Buffer);
-  Screen->drawHLine(MapX(NumericLineX), MapY(NumericLineY), NumericLineWidth);
+  Screen->drawStr(TextX, TextY, Buffer);
+  Screen->drawBox(UnderscoreX, UnderscoreY, UnderscoreWidth, UnderscoreHeight);
 }
 
 void StatePrinter::DisplayNotConnectedError()
