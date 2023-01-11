4f2ecaae502fc76d091c897b0ee93ff1a1cc2d22
Ian <ian@somewhere.com>
2023-01-10 22:22:37 -0500

    [?^] [ultrasonic_distance_meter] Even more fancier progress display.

--- ultrasonic_distance_meter/ultrasonic_distance_meter.ino
+++ ultrasonic_distance_meter/ultrasonic_distance_meter.ino
@@ -14,8 +14,8 @@
 */
 
 /*
-  Version: 5
-  Last mod.: 2023-01-09
+  Version: 6
+  Last mod.: 2023-01-10
 */
 
 const uint8_t
@@ -46,16 +46,20 @@ void setup()
   screen.begin();
   screen.setContrast(10);
 
+  screen.clearBuffer();
+  screen.drawFrame(0, 0, 128, 64);
+  screen.sendBuffer();
+
   Serial.begin(57600);
 }
 
 void loop()
 {
   const uint8_t
-    MeasurementsPerCycle = 24; // 48
+    MeasurementsPerCycle = 12; // 48
 
   const uint32_t
-    CycleDurationMs = 1000;  // 800
+    CycleDurationMs = 800;  // 800
 
   const uint32_t
     MeasurementsDelayMs = CycleDurationMs / MeasurementsPerCycle;
@@ -100,7 +104,10 @@ void loop()
   bool isConnected = (numNotConnectedErrors < MeasurementsPerCycle);
   bool hasDistance = (numSuccessfullMeasurements > 0);
 
-  screen.clearBuffer();
+  screen.setDrawColor(0);
+  screen.drawBox(1, 1, 126, 62);
+  screen.setDrawColor(1);
+
   _StatePrinter.Display(isConnected, hasDistance, minDistance);
   screen.sendBuffer();
 }
@@ -138,8 +145,6 @@ void displayProgress(
   */
 
   // Display progress as dots in ellipse with center in display (
-  screen.drawFrame(0, 0, 128, 64);
-
   const uint8_t ellipseCenterX = 128 / 2 - 1;
   const uint8_t ellipseCenterY = 64 / 2 - 1;
   const uint8_t ellipseSizeX = 128 - 12;
@@ -147,15 +152,23 @@ void displayProgress(
 
   u8g2_uint_t x, y;
 
-  float angleRad = ((float) currentStep / totalSteps) * TWO_PI - PI / 2;
+  float progress = (float) currentStep / totalSteps;
+  progress = progress + ((float) random(100) / 100) * 0.6 / totalSteps;
+  float angleRad = progress * TWO_PI - PI / 2;
 
-  x = ellipseCenterX + cos(angleRad) * ellipseSizeX / 2;
-  y = ellipseCenterY + sin(angleRad) * ellipseSizeY / 2;
+  float cosA = cos(angleRad);
+  float sinA = sin(angleRad);
+  float powCosA = copysign(pow(abs(cosA), 1.75), cosA);
+  float powSinA = copysign(pow(abs(sinA), 0.5), sinA);
+  x = (float) (ellipseSizeX / 2) * powCosA + ellipseCenterX;
+  y = (float) (ellipseSizeY / 2) * powSinA + ellipseCenterY;
 
   if (hasDistance)
     screen.drawDisc(x, y, 4);
   else
     screen.drawCircle(x, y, 4);
+
+  // screen.drawLine(ellipseCenterX, ellipseCenterY, x, y);
 
   screen.sendBuffer();
   // )
