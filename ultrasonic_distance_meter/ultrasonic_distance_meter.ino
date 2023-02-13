25eff53bd3481fc9a4ba194d5a87136ffdd12db9
Ian <ian@somewhere.com>
2023-02-12 19:03:34 -0500

    [-] [ultrasonic_distance_meter] Dropped that fancy stuff.
    Minimalistic elegance.

--- ultrasonic_distance_meter/ultrasonic_distance_meter.ino
+++ ultrasonic_distance_meter/ultrasonic_distance_meter.ino
@@ -1,21 +1,23 @@
-// Ultrasonic distance meter
+// Ultrasonic distance meter.
 
 /*
-  Hardware used:
-
-    Controller: ATmega328P
-    Distance meter: HC-SR04
-    Display: SH1106 128x64 I2C
-
-  Wiring:
-
-    TriggerPin: "Trig"
-    EchoPin: "Echo"
+  Version: 7
+  Last mod.: 2023-02-12
 */
 
 /*
-  Version: 6
-  Last mod.: 2023-01-10
+  Hardware used
+
+    Distance sensor: HC-SR04
+    Controller: ATmega328P
+    Display: SH1106 128x64 I2C
+
+  Wiring
+
+    "Trig" == TriggerPin
+    "Echo" == EchoPin
+
+    You can wire Trig and Echo to one pin.
 */
 
 const uint8_t
@@ -49,20 +51,12 @@ void setup()
   screen.clearBuffer();
   screen.drawFrame(0, 0, 128, 64);
   screen.sendBuffer();
-
-  Serial.begin(57600);
 }
 
 void loop()
 {
-  const uint8_t
-    MeasurementsPerCycle = 12; // 48
-
-  const uint32_t
-    CycleDurationMs = 800;  // 800
-
-  const uint32_t
-    MeasurementsDelayMs = CycleDurationMs / MeasurementsPerCycle;
+  const uint8_t MeasurementsPerCycle = 16;
+  const uint32_t MeasurementsDelayMs = 10;
 
   float minDistance = 1e6;
   float maxDistance = -1.0;
@@ -90,14 +84,6 @@ void loop()
       maxDistance = max(maxDistance, result_Distance);
     }
 
-    displayProgress(
-      i + 1,
-      MeasurementsPerCycle,
-      result_NotConnected,
-      result_HasDistance,
-      result_Distance
-    );
-
     delay(MeasurementsDelayMs);
   }
 
@@ -110,66 +96,4 @@ void loop()
 
   _StatePrinter.Display(isConnected, hasDistance, minDistance);
   screen.sendBuffer();
-}
-
-void displayProgress(
-  uint8_t currentStep,
-  uint8_t totalSteps,
-  bool isNotConnected,
-  bool hasDistance,
-  float distanceCm
-)
-{
-  /*
-  Serial.println("Progress:");
-
-  Serial.print("  Measurement: ");
-  Serial.print(currentStep);
-  Serial.print(" / ");
-  Serial.print(totalSteps);
-  Serial.println();
-
-  Serial.print("  Is connected: ");
-  Serial.print(!isNotConnected);
-  Serial.println();
-
-  Serial.print("  Has distance: ");
-  Serial.print(hasDistance);
-  Serial.println();
-
-  Serial.print("  Distance (cm): ");
-  Serial.print(distanceCm);
-  Serial.println();
-
-  Serial.println();
-  */
-
-  // Display progress as dots in ellipse with center in display (
-  const uint8_t ellipseCenterX = 128 / 2 - 1;
-  const uint8_t ellipseCenterY = 64 / 2 - 1;
-  const uint8_t ellipseSizeX = 128 - 12;
-  const uint8_t ellipseSizeY = 64 - 12;
-
-  u8g2_uint_t x, y;
-
-  float progress = (float) currentStep / totalSteps;
-  progress = progress + ((float) random(100) / 100) * 0.6 / totalSteps;
-  float angleRad = progress * TWO_PI - PI / 2;
-
-  float cosA = cos(angleRad);
-  float sinA = sin(angleRad);
-  float powCosA = copysign(pow(abs(cosA), 1.75), cosA);
-  float powSinA = copysign(pow(abs(sinA), 0.5), sinA);
-  x = (float) (ellipseSizeX / 2) * powCosA + ellipseCenterX;
-  y = (float) (ellipseSizeY / 2) * powSinA + ellipseCenterY;
-
-  if (hasDistance)
-    screen.drawDisc(x, y, 4);
-  else
-    screen.drawCircle(x, y, 4);
-
-  // screen.drawLine(ellipseCenterX, ellipseCenterY, x, y);
-
-  screen.sendBuffer();
-  // )
 }
