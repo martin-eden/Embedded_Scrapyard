54544e76e7b2013cf7971ffbe664926e5648faef
Ian <ian@somewhere.com>
2024-03-19 18:54:30 +0000

    [?/] [test_something]

--- tests/test_something/test_something.ino
+++ tests/test_something/test_something.ino
@@ -1,22 +1,18 @@
-template <typename type> int8_t sign(type value)
-{
-  return type((value > 0) - (value < 0));
-}
+#include <me_Types.h>
 
 void doSomething()
 {
-  int8_t Power = -100;
-  uint8_t MotorPower;
-
-  MotorPower = map(abs(Power), 0, 100, 0, 255);
-
-  Serial.print("Power: ");
-  Serial.print(Power);
-  Serial.println();
-
-  Serial.print("MotorPower: ");
-  Serial.print(MotorPower);
-  Serial.println();
+  TUint_1 a, b, seed, d;
+  seed = analogRead(A0);
+  randomSeed(seed);
+  a = random();
+  a = 123;
+  b = random();
+  d = a / b;
+  Serial.println(seed);
+  Serial.println(a);
+  Serial.println(b);
+  Serial.println(d);
 }
 
 void setup()
