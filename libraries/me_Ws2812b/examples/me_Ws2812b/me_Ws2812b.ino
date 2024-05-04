e686bd7361735d451ded58a0797f2bf3b8903f93
Ian <ian@somewhere.com>
2024-05-04 15:35:42 +0200

    [?] [me_Ws2812b.example] SendBytes() are removed..
    .. need to rewrite tests using SendPixels(). Weird workaround, but
    those tests are weird too. Still useful for debug/verification.

--- libraries/me_Ws2812b/examples/me_Ws2812b/me_Ws2812b.ino
+++ libraries/me_Ws2812b/examples/me_Ws2812b/me_Ws2812b.ino
@@ -18,7 +18,7 @@
 
   Author: Martin Eden
   Development: 2024-02/2024-04
-  Last mod.: 2024-04-23
+  Last mod.: 2024-05-04
 */
 
 #include <me_Types.h> // TUint's
@@ -87,7 +87,7 @@ void Test_ObserveBitsTiming()
       0x00,
     };
 
-  me_Ws2812b::SendBytes(TestPacket, sizeof(TestPacket), LedStripePin);
+  // me_Ws2812b::SendBytes(TestPacket, sizeof(TestPacket), LedStripePin);
 
   delay(5000);
 }
@@ -120,7 +120,7 @@ void Test_ObserveColorsOrder()
   TestPacket[TestPacket_Size - 2] = 0x00;
   TestPacket[TestPacket_Size - 1] = 0xFF;
 
-  me_Ws2812b::SendBytes(TestPacket, TestPacket_Size, LedStripePin);
+  // me_Ws2812b::SendBytes(TestPacket, TestPacket_Size, LedStripePin);
 
   delay(5000);
 }
